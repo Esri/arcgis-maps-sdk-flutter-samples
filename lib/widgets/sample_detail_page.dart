570b245443fcff8e07b9586e0335e99d7b336ea1
Jen Merritt <jmerritt@esri.com>
2024-06-05 11:44:45 +0100

    Create sample_detail_page.dart

---
+++ lib/widgets/sample_detail_page.dart
@@ -0,0 +1,21 @@
+import 'package:arcgis_maps_sdk_flutter_samples/models/sample.dart';
+import 'package:flutter/material.dart';
+
+class SampleDetailPage extends StatelessWidget {
+  const SampleDetailPage({super.key, required this.sample});
+
+  final Sample sample;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Text(sample.title),
+        ),
+      ),
+      body: sample.getSampleWidget(),
+    );
+  }
+}
