2a327183411aa50266839340e903e56531681644
Jen Merritt <jmerritt@esri.com>
2024-06-12 11:31:28 +0100

    Update readme_scripts_runner.dart

--- tool/readme_scripts/readme_scripts_runner.dart
+++ tool/readme_scripts/readme_scripts_runner.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: avoid_print
-//
 // Copyright 2024 Esri
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// ignore_for_file: avoid_print
 
 import 'dart:io';
 
