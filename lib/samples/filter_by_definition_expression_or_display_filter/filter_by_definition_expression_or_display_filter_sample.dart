45509f594e56d22803969556134783acea17fd0a
Shokhinabonu Tojieva <stojieva@esri.com>
2024-06-20 16:00:43 -0700

    Added feature count

--- lib/samples/filter_by_definition_expression_or_display_filter/filter_by_definition_expression_or_display_filter_sample.dart
+++ lib/samples/filter_by_definition_expression_or_display_filter/filter_by_definition_expression_or_display_filter_sample.dart
@@ -27,48 +27,60 @@ class FilterByDefinitionExpressionOrDisplayFilterSample extends StatefulWidget {
 
 class _FilterByDefinitionExpressionOrDisplayFilterSampleState
     extends State<FilterByDefinitionExpressionOrDisplayFilterSample> {
+  // create a map view controller
   final _mapViewController = ArcGISMapView.createController();
-  final _featureLayer = FeatureLayer.withFeatureTable(
+  final FeatureLayer _featureLayer = FeatureLayer.withFeatureTable(
       ServiceFeatureTable.withUri(Uri.parse(
           'https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/SF_311_Incidents/FeatureServer/0')));
 
   @override
   void initState() {
     super.initState();
-
+    // create a map with a basemap style
     final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
+    // add the feature layer to the map
     map.operationalLayers.add(_featureLayer);
+    // set the initial viewpoint
     map.initialViewpoint = Viewpoint.withLatLongScale(
       latitude: 37.7759,
       longitude: -122.45044,
       scale: 100000,
     );
 
+    // set the map to the map view.
     _mapViewController.arcGISMap = map;
+
+    calculateFeatureCount();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+        // add a column to the scaffold
         child: Column(
           children: [
             Expanded(
+              // add the map view to the column
               child: ArcGISMapView(
                 controllerProvider: () => _mapViewController,
               ),
             ),
+            // add a text widget to display the feature count
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                // add a button to apply a definition expression
                 TextButton(
                   onPressed: applyDefinitionExpression,
                   child: const Text('Definition Expression'),
                 ),
+                // add a button to apply a display filter
                 TextButton(
                   onPressed: applyDisplayFilter,
                   child: const Text('Display Filter'),
                 ),
+                // add a button to reset the definition expression and display filter
                 TextButton(
                   onPressed: reset,
                   child: const Text('Reset'),
@@ -82,24 +94,58 @@ class _FilterByDefinitionExpressionOrDisplayFilterSampleState
   }
 
   void applyDefinitionExpression() {
+    // remove the display filter
     _featureLayer.displayFilterDefinition = null;
+    // apply a definition expression to the feature layer
     _featureLayer.definitionExpression =
         "req_Type = 'Tree Maintenance or Damage'";
+    // count the number of features
+    calculateFeatureCount();
   }
 
   void applyDisplayFilter() {
+    // remove the definition expression
     _featureLayer.definitionExpression = '';
+    // apply a display filter to the feature layer
     final displayFilter = DisplayFilter.withWhereClause(
         name: 'Damaged Trees',
         whereClause: "req_type LIKE '%Tree Maintenance%'");
+    // create a manual display filter definition
     final manualDisplayFilterDefinition =
         ManualDisplayFilterDefinition.withFilters(
             activeFilter: displayFilter, availableFilters: [displayFilter]);
     _featureLayer.displayFilterDefinition = manualDisplayFilterDefinition;
+    // count the number of features
+    calculateFeatureCount();
   }
 
   void reset() {
+    // remove the definition expression and display filter
     _featureLayer.displayFilterDefinition = null;
     _featureLayer.definitionExpression = '';
+    // count the number of features
+    calculateFeatureCount();
+  }
+
+  void calculateFeatureCount() async {
+    // count the number of features
+    final queryParameters = QueryParameters();
+    queryParameters.geometry = _mapViewController
+        .getCurrentViewpoint(viewpointType: ViewpointType.boundingGeometry)
+        ?.targetGeometry
+        .extent;
+    queryParameters.whereClause = '1=1';
+
+    final featureCount = await _featureLayer.featureTable!
+        .queryFeatureCount(queryParameters: queryParameters);
+
+    if (mounted) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(content: Text('$featureCount features found.'));
+        },
+      );
+    }
   }
 }
