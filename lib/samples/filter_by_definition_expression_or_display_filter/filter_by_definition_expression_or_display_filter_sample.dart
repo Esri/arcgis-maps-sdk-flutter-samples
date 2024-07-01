0f49c5750460a0d2979d0d97357ec9bdcf1aa2f9
Shokhinabonu Tojieva <stojieva@esri.com>
2024-07-01 09:56:19 -0700

    Filter by Definition Expression or Display Filter Sample (#35)
    * Added feature count
    
    * Added the countFeatureTable variable that displays the count too, added both README files and comments
    
    * Refactored code to have all the map initializations inside onMapViewReady
    
    * Refactored code and variable names

--- lib/samples/filter_by_definition_expression_or_display_filter/filter_by_definition_expression_or_display_filter_sample.dart
+++ lib/samples/filter_by_definition_expression_or_display_filter/filter_by_definition_expression_or_display_filter_sample.dart
@@ -27,48 +27,46 @@ class FilterByDefinitionExpressionOrDisplayFilterSample extends StatefulWidget {
 
 class _FilterByDefinitionExpressionOrDisplayFilterSampleState
     extends State<FilterByDefinitionExpressionOrDisplayFilterSample> {
+  // create a map view controller
   final _mapViewController = ArcGISMapView.createController();
+  // create a feature layer
   final _featureLayer = FeatureLayer.withFeatureTable(
       ServiceFeatureTable.withUri(Uri.parse(
           'https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/SF_311_Incidents/FeatureServer/0')));
-
-  @override
-  void initState() {
-    super.initState();
-
-    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
-    map.operationalLayers.add(_featureLayer);
-    map.initialViewpoint = Viewpoint.withLatLongScale(
-      latitude: 37.7759,
-      longitude: -122.45044,
-      scale: 100000,
-    );
-
-    _mapViewController.arcGISMap = map;
-  }
+  // create a display filter definition
+  ManualDisplayFilterDefinition? _displayFilterDefinition;
+  // create a definition expression
+  var _definitionExpression = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+        top: false,
         child: Column(
           children: [
             Expanded(
+              // add a map view to the widget tree and set a controller.
               child: ArcGISMapView(
                 controllerProvider: () => _mapViewController,
+                onMapViewReady: onMapViewReady,
               ),
             ),
+            // add a text widget to display the feature count
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                // add a button to apply a definition expression
                 TextButton(
                   onPressed: applyDefinitionExpression,
                   child: const Text('Definition Expression'),
                 ),
+                // add a button to apply a display filter
                 TextButton(
                   onPressed: applyDisplayFilter,
                   child: const Text('Display Filter'),
                 ),
+                // add a button to reset the definition expression and display filter
                 TextButton(
                   onPressed: reset,
                   child: const Text('Reset'),
@@ -81,25 +79,91 @@ class _FilterByDefinitionExpressionOrDisplayFilterSampleState
     );
   }
 
-  void applyDefinitionExpression() {
-    _featureLayer.displayFilterDefinition = null;
-    _featureLayer.definitionExpression =
-        "req_Type = 'Tree Maintenance or Damage'";
+  void onMapViewReady() {
+    // create a map with a topographic basemap style
+    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
+    // add the feature layer to the map
+    map.operationalLayers.add(_featureLayer);
+    // set the initial viewpoint
+    map.initialViewpoint = Viewpoint.withLatLongScale(
+      latitude: 37.7759,
+      longitude: -122.45044,
+      scale: 100000,
+    );
+
+    // set the map to the map view.
+    _mapViewController.arcGISMap = map;
   }
 
-  void applyDisplayFilter() {
-    _featureLayer.definitionExpression = '';
+  void applyDefinitionExpression() async {
+    // remove the display filter
+    _displayFilterDefinition = null;
+    // apply a definition expression to the feature layer
+    _definitionExpression = "req_Type = 'Tree Maintenance or Damage'";
+    // count the number of features
+    await calculateFeatureCount();
+  }
+
+  void applyDisplayFilter() async {
+    // remove the definition expression
+    _definitionExpression = '';
+    // apply a display filter to the feature layer
     final displayFilter = DisplayFilter.withWhereClause(
         name: 'Damaged Trees',
         whereClause: "req_type LIKE '%Tree Maintenance%'");
+    // create a manual display filter definition
     final manualDisplayFilterDefinition =
         ManualDisplayFilterDefinition.withFilters(
             activeFilter: displayFilter, availableFilters: [displayFilter]);
-    _featureLayer.displayFilterDefinition = manualDisplayFilterDefinition;
+    _displayFilterDefinition = manualDisplayFilterDefinition;
+    // count the number of features
+    await calculateFeatureCount();
   }
 
-  void reset() {
-    _featureLayer.displayFilterDefinition = null;
-    _featureLayer.definitionExpression = '';
+  void reset() async {
+    // remove the definition expression and display filter
+    _displayFilterDefinition = null;
+    _definitionExpression = '';
+    // count the number of features
+    await calculateFeatureCount();
+  }
+
+  Future<void> calculateFeatureCount() async {
+    _featureLayer.displayFilterDefinition = _displayFilterDefinition;
+    _featureLayer.definitionExpression = _definitionExpression;
+    // get the current extent of the map view
+    final extent = _mapViewController
+        .getCurrentViewpoint(viewpointType: ViewpointType.boundingGeometry)
+        ?.targetGeometry
+        .extent;
+
+    // create query parameters
+    final queryParameters = QueryParameters();
+    queryParameters.geometry = extent;
+
+    // query the feature count
+    final featureCount = await _featureLayer.featureTable!
+        .queryFeatureCount(queryParameters: queryParameters);
+
+    // show the feature count in an alert dialog
+    if (mounted) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          // create an alert dialog
+          return AlertDialog(
+            title: const Text(
+              'Current Feature Count',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            content: Text(
+              '$featureCount features',
+              textAlign: TextAlign.center,
+            ),
+          );
+        },
+      );
+    }
   }
 }
