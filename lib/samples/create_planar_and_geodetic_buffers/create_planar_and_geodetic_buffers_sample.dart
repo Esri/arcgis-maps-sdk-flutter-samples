c55380ce5a243b893b0a2ffcfe97e230fb139e99
Paul Sturm <psturm@esri.com>
2024-07-18 14:17:47 -0500

    draw the buffers

--- lib/samples/create_planar_and_geodetic_buffers/create_planar_and_geodetic_buffers_sample.dart
+++ lib/samples/create_planar_and_geodetic_buffers/create_planar_and_geodetic_buffers_sample.dart
@@ -32,6 +32,12 @@ class _CreatePlanarAndGeodeticBuffersSampleState
     with SampleStateSupport {
   // Create a controller for the map view.
   final _mapViewController = ArcGISMapView.createController();
+  // The graphics overlay for the geodetic buffers.
+  final _geodeticOverlay = GraphicsOverlay();
+  // The graphics overlay for the planar buffers.
+  final _planarOverlay = GraphicsOverlay();
+  // The graphics overlay for the tapped points.
+  final _pointOverlay = GraphicsOverlay();
   // A flag for when the map view is ready and controls can be used.
   var _ready = false;
 
@@ -55,10 +61,10 @@ class _CreatePlanarAndGeodeticBuffersSampleState
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    // A button to perform a task.
+                    // A button to clear the buffers.
                     ElevatedButton(
-                      onPressed: performTask,
-                      child: const Text('Perform Task'),
+                      onPressed: clear,
+                      child: const Text('Clear'),
                     ),
                   ],
                 ),
@@ -80,24 +86,96 @@ class _CreatePlanarAndGeodeticBuffersSampleState
     );
   }
 
-  void onMapViewReady() async {
+  void onMapViewReady() {
+    // Configure the graphics overlay for the geodetic buffers.
+    _geodeticOverlay.renderer = SimpleRenderer(
+      symbol: SimpleFillSymbol(
+        style: SimpleFillSymbolStyle.solid,
+        color: Colors.green,
+        outline: SimpleLineSymbol(
+          style: SimpleLineSymbolStyle.solid,
+          color: Colors.black,
+          width: 2.0,
+        ),
+      ),
+    );
+    _geodeticOverlay.opacity = 0.5;
+
+    // Configure the graphics overlay for the planar buffers.
+    _planarOverlay.renderer = SimpleRenderer(
+      symbol: SimpleFillSymbol(
+        style: SimpleFillSymbolStyle.solid,
+        color: Colors.red,
+        outline: SimpleLineSymbol(
+          style: SimpleLineSymbolStyle.solid,
+          color: Colors.black,
+          width: 2.0,
+        ),
+      ),
+    );
+    _planarOverlay.opacity = 0.5;
+
+    // Configure the graphics overlay for the tapped points.
+    _pointOverlay.renderer = SimpleRenderer(
+      symbol: SimpleMarkerSymbol(
+        style: SimpleMarkerSymbolStyle.cross,
+        color: Colors.white,
+        size: 14.0,
+      ),
+    );
+
+    // Add the overlays to the map view.
+    _mapViewController.graphicsOverlays.addAll(
+      [
+        _geodeticOverlay,
+        _planarOverlay,
+        _pointOverlay,
+      ],
+    );
+
+    // Create a map with the topographic basemap style and set to the map view.
     final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
     _mapViewController.arcGISMap = map;
-    // Perform some long-running setup task.
-    await Future.delayed(const Duration(seconds: 10));
+
     // Set the ready state variable to true to enable the sample UI.
     setState(() => _ready = true);
   }
 
-  void onTap(Offset offset) {
-    print('Tapped at $offset');
+  void onTap(Offset screenPoint) {
+    // Capture the tapped point and convert it to a map point.
+    final mapPoint = _mapViewController.screenToLocation(screen: screenPoint);
+    if (mapPoint == null) return;
+
+    // Create a geodetic buffer around the tapped point at the specified distance.
+    final geodeticGeometry = GeometryEngine.bufferGeodetic(
+      geometry: mapPoint,
+      distance: 1000000.0, //fixme
+      distanceUnit: LinearUnit(unitId: LinearUnitId.meters),
+      maxDeviation: double.nan,
+      curveType: GeodeticCurveType.geodesic,
+    );
+    // Create and add a graphic to the geodetic overlay.
+    final geodeticGraphic = Graphic(geometry: geodeticGeometry);
+    _geodeticOverlay.graphics.add(geodeticGraphic);
+
+    // Create a planar buffer around the tapped point at the specified distance.
+    final planarGeometry = GeometryEngine.buffer(
+      geometry: mapPoint,
+      distance: 1000000.0, //fixme
+    );
+    // Create and add a graphic to the planar overlay.
+    final planarGraphic = Graphic(geometry: planarGeometry);
+    _planarOverlay.graphics.add(planarGraphic);
+
+    // Create and add a graphic to the point overlay.
+    final pointGraphic = Graphic(geometry: mapPoint);
+    _pointOverlay.graphics.add(pointGraphic);
   }
 
-  void performTask() async {
-    setState(() => _ready = false);
-    // Perform some task.
-    print('Perform task');
-    await Future.delayed(const Duration(seconds: 5));
-    setState(() => _ready = true);
+  // Clear the graphics overlays.
+  void clear() {
+    _geodeticOverlay.graphics.clear();
+    _planarOverlay.graphics.clear();
+    _pointOverlay.graphics.clear();
   }
 }
