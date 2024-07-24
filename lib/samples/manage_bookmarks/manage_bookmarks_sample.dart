c5879d8af638d6985fe13575d126814f1ee2041f
Paul Sturm <psturm@esri.com>
2024-07-24 13:54:46 -0500

    select bookmark

--- lib/samples/manage_bookmarks/manage_bookmarks_sample.dart
+++ lib/samples/manage_bookmarks/manage_bookmarks_sample.dart
@@ -14,6 +14,7 @@
 // limitations under the License.
 //
 
+import 'dart:math';
 import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:flutter/material.dart';
 
@@ -27,8 +28,11 @@ class ManageBookmarksSample extends StatefulWidget {
 class _ManageBookmarksSampleState extends State<ManageBookmarksSample> {
   // Create a controller for the map view.
   final _mapViewController = ArcGISMapView.createController();
+  late final List<Bookmark> _bookmarks;
   // A flag for when the map view is ready and controls can be used.
   var _ready = false;
+  // A flag for when the bookmarks bottom sheet is visible.
+  var _bookmarksVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -44,16 +48,20 @@ class _ManageBookmarksSampleState extends State<ManageBookmarksSample> {
                   child: ArcGISMapView(
                     controllerProvider: () => _mapViewController,
                     onMapViewReady: onMapViewReady,
-                    onTap: onTap,
                   ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    // A button to perform a task.
+                    // A button to add a bookmark at the current Viewpoint.
                     ElevatedButton(
-                      onPressed: performTask,
-                      child: const Text('Perform Task'),
+                      onPressed: addBookmark,
+                      child: const Text('Add Bookmark'),
+                    ),
+                    // A button to list the bookmarks.
+                    ElevatedButton(
+                      onPressed: () => setState(() => _bookmarksVisible = true),
+                      child: const Text('Bookmarks'),
                     ),
                   ],
                 ),
@@ -72,23 +80,101 @@ class _ManageBookmarksSampleState extends State<ManageBookmarksSample> {
           ],
         ),
       ),
+      // The Bookmarks bottom sheet.
+      bottomSheet: _bookmarksVisible ? buildBookmarks(context) : null,
     );
   }
 
-  void onMapViewReady() async {
-    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
+  // The build method for the Bookmarks bottom sheet.
+  Widget buildBookmarks(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.fromLTRB(
+        20.0,
+        0.0,
+        20.0,
+        max(
+          20.0,
+          View.of(context).viewPadding.bottom /
+              View.of(context).devicePixelRatio,
+        ),
+      ),
+      //fixme scrollview?
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              Text(
+                'Bookmarks',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const Spacer(),
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () => setState(() => _bookmarksVisible = false),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20.0),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: _bookmarks.length,
+            itemBuilder: (context, index) {
+              return TextButton(
+                onPressed: () =>
+                    _mapViewController.setBookmark(_bookmarks[index]),
+                style: const ButtonStyle(
+                  alignment: Alignment.centerLeft,
+                ),
+                child: Text(_bookmarks[index].name),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onMapViewReady() {
+    //fixme comments
+    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISImagery);
+
+    _bookmarks = map.bookmarks;
+
+    _bookmarks.addAll([
+      Bookmark(
+        name: 'Grand Prismatic Spring',
+        viewpoint: Viewpoint.withLatLongScale(
+            latitude: 44.525, longitude: -110.838, scale: 6e3),
+      ),
+      Bookmark(
+        name: 'Guitar-Shaped Forest',
+        viewpoint: Viewpoint.withLatLongScale(
+            latitude: -33.867, longitude: -63.985, scale: 4e4),
+      ),
+      Bookmark(
+        name: 'Mysterious Desert Pattern',
+        viewpoint: Viewpoint.withLatLongScale(
+            latitude: 27.380, longitude: 33.632, scale: 6e3),
+      ),
+      Bookmark(
+        name: 'Strange Symbol',
+        viewpoint: Viewpoint.withLatLongScale(
+            latitude: 37.401, longitude: -116.867, scale: 6e3),
+      ),
+    ]);
+
     _mapViewController.arcGISMap = map;
-    // Perform some long-running setup task.
-    await Future.delayed(const Duration(seconds: 10));
+
+    _mapViewController.setBookmark(_bookmarks.first);
+
     // Set the ready state variable to true to enable the sample UI.
     setState(() => _ready = true);
   }
 
-  void onTap(Offset offset) {
-    print('Tapped at $offset');
-  }
-
-  void performTask() async {
+  void addBookmark() async {
+    //fixme
     setState(() => _ready = false);
     // Perform some task.
     print('Perform task');
