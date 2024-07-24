94b14f559988c8c5c21bdd6218983595ce79c005
kmueller-gis <kmueller@esri.com>
2024-07-24 15:44:10 -0500

    Sample at initial state
    - Sample added to the sample listing
    - initial graphics added
    - Initial viewpoint set to graphic envelope

--- lib/samples/cut_geometry/cut_geometry_sample.dart
+++ lib/samples/cut_geometry/cut_geometry_sample.dart
@@ -17,6 +17,10 @@ class _CutGeometrySampleState extends State<CutGeometrySample>
   // A flag for when the map view is ready and controls can be used.
   var _ready = false;
 
+  late final Graphic _lakeGraphic;
+  late final Graphic _borderGraphic;
+  final graphicsOverlay = GraphicsOverlay();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +44,7 @@ class _CutGeometrySampleState extends State<CutGeometrySample>
                     // A button to perform a task.
                     ElevatedButton(
                       onPressed: performTask,
-                      child: const Text('Perform Task'),
+                      child: const Text('Cut'),
                     ),
                   ],
                 ),
@@ -65,8 +69,15 @@ class _CutGeometrySampleState extends State<CutGeometrySample>
   void onMapViewReady() async {
     final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
     _mapViewController.arcGISMap = map;
-    // Perform some long-running setup task.
-    await Future.delayed(const Duration(seconds: 10));
+
+    // Initialize the graphics for the sample.
+    initializeGraphics();
+
+    // Set the viewpoint to the lake graphic.
+    _mapViewController.setViewpoint(
+      Viewpoint.fromTargetExtent(_lakeGraphic.geometry!.extent),
+    );
+
     // Set the ready state variable to true to enable the sample UI.
     setState(() => _ready = true);
   }
@@ -81,5 +92,125 @@ class _CutGeometrySampleState extends State<CutGeometrySample>
     print('Perform task');
     await Future.delayed(const Duration(seconds: 5));
     setState(() => _ready = true);
+  }
+
+  void initializeGraphics() {
+    _initLakeGraphic();
+    _initBorderGraphic();
+
+    // Add the graphics to the graphics overlay.
+    graphicsOverlay.graphics.addAll([_lakeGraphic, _borderGraphic]);
+
+    // Add the graphics overlay to the map view controller.
+    _mapViewController.graphicsOverlays.add(graphicsOverlay);
+  }
+
+  void _initLakeGraphic() {
+    // Build the Polygon for the lake.
+    final lakePolygonBuilder = PolygonBuilder.fromSpatialReference(
+        _mapViewController.spatialReference);
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -10254374.668616, y: 5908345.076380));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -10178382.525314, y: 5971402.386779));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -10118558.923141, y: 6034459.697178));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9993252.729399, y: 6093474.872295));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9882498.222673, y: 6209888.368416));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9821057.766387, y: 6274562.532928));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9690092.583250, y: 6241417.023616));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9605207.742329, y: 6206654.660191));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9564786.389509, y: 6108834.986367));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9449989.747500, y: 6095091.726408));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9462116.153346, y: 6044160.821855));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9417652.665244, y: 5985145.646738));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9438671.768711, y: 5946341.148031));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9398250.415891, y: 5922088.336339));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9419269.519357, y: 5855797.317714));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9467775.142741, y: 5858222.598884));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9462924.580403, y: 5902686.086985));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9598740.325877, y: 5884092.264688));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9643203.813979, y: 5845287.765981));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9739406.633691, y: 5879241.702350));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9783061.694736, y: 5922896.763395));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9844502.151022, y: 5936640.023354));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9773360.570059, y: 6019099.583107));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9883306.649729, y: 5968977.105610));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -9957681.938918, y: 5912387.211662));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -10055501.612742, y: 5871965.858842));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -10116942.069028, y: 5884092.264688));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -10111283.079633, y: 5933406.315128));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -10214761.742852, y: 5888134.399970));
+    lakePolygonBuilder
+        .addPoint(ArcGISPoint(x: -10254374.668616, y: 5901877.659929));
+
+    // Generate the polygon graphic.
+    _lakeGraphic = Graphic(
+      geometry: lakePolygonBuilder.toGeometry(),
+      symbol: SimpleFillSymbol(
+        color: Colors.blue.withOpacity(0.5),
+        outline: SimpleLineSymbol(
+          color: Colors.blue,
+          width: 4.0,
+          style: SimpleLineSymbolStyle.solid,
+        ),
+      ),
+    );
+  }
+
+  void _initBorderGraphic() {
+    // Build the Polyline geometry for the border.
+    final borderPolylineBuilder = PolylineBuilder.fromSpatialReference(
+        _mapViewController.spatialReference);
+    borderPolylineBuilder
+        .addPoint(ArcGISPoint(x: -9981328.687124, y: 6111053.281447));
+    borderPolylineBuilder
+        .addPoint(ArcGISPoint(x: -9946518.044066, y: 6102350.620682));
+    borderPolylineBuilder
+        .addPoint(ArcGISPoint(x: -9872545.427566, y: 6152390.920079));
+    borderPolylineBuilder
+        .addPoint(ArcGISPoint(x: -9838822.617103, y: 6157830.083057));
+    borderPolylineBuilder
+        .addPoint(ArcGISPoint(x: -9446115.050097, y: 5927209.572793));
+    borderPolylineBuilder
+        .addPoint(ArcGISPoint(x: -9430885.393759, y: 5876081.440801));
+    borderPolylineBuilder
+        .addPoint(ArcGISPoint(x: -9415655.737420, y: 5860851.784463));
+
+    // Generate the polyline graphic.
+    _borderGraphic = Graphic(
+      geometry: borderPolylineBuilder.toGeometry(),
+      symbol: SimpleLineSymbol(
+        color: Colors.red,
+        width: 4.0,
+        style: SimpleLineSymbolStyle.dot,
+      ),
+    );
   }
 }
