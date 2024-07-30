48a504c431657e6984249cec03a89a018208741a
Changan Shi <cshi@esri.com>
2024-07-30 13:31:47 -0700

    Merge pull request #97 from Esri/cs/1561_download_vector
    " Download Vector Tiles To Local Cache" new Sample

--- lib/models/samples_widget_list.dart
+++ lib/models/samples_widget_list.dart
@@ -18,6 +18,7 @@ import 'package:arcgis_maps_sdk_flutter_samples/samples/display_clusters/display
 import 'package:arcgis_maps_sdk_flutter_samples/samples/display_map/display_map_sample.dart';
 import 'package:arcgis_maps_sdk_flutter_samples/samples/display_map_from_mobile_map_package/display_map_from_mobile_map_package_sample.dart';
 import 'package:arcgis_maps_sdk_flutter_samples/samples/download_preplanned_map_area/download_preplanned_map_area_sample.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/samples/download_vector_tiles_to_local_cache/download_vector_tiles_to_local_cache_sample.dart';
 import 'package:arcgis_maps_sdk_flutter_samples/samples/edit_feature_attachments/edit_feature_attachments_sample.dart';
 import 'package:arcgis_maps_sdk_flutter_samples/samples/filter_by_definition_expression_or_display_filter/filter_by_definition_expression_or_display_filter_sample.dart';
 import 'package:arcgis_maps_sdk_flutter_samples/samples/find_address_with_reverse_geocode/find_address_with_reverse_geocode_sample.dart';
@@ -68,6 +69,8 @@ const sampleWidgets = <String, Widget>{
   'display_map': DisplayMapSample(),
   'display_map_from_mobile_map_package': DisplayMapFromMobileMapPackageSample(),
   'download_preplanned_map_area': DownloadPreplannedMapAreaSample(),
+  'download_vector_tiles_to_local_cache':
+      DownloadVectorTilesToLocalCacheSample(),
   'edit_feature_attachments': EditFeatureAttachmentsSample(),
   'filter_by_definition_expression_or_display_filter':
       FilterByDefinitionExpressionOrDisplayFilterSample(),
