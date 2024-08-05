f7aea83b18d1826e9f6a564e6585fe8da73ff3cd
Jen Merritt <jmerritt@esri.com>
2024-08-05 14:49:26 +0100

    Apply Common Design to Authenticate with OAuth Sample (#109)
    * Apply common design and patterns to sample
    
    * Add screen captures
    
    * Create README.md
    
    * generate metadata
    
    * Update authenticate_with_oauth.dart
    
    * Add image
    
    * Remove space
    
    * Add comment updates to show portal user info
    
    * Add comment to authenticate with token

--- lib/samples/authenticate_with_oauth/authenticate_with_oauth.dart
+++ lib/samples/authenticate_with_oauth/authenticate_with_oauth.dart
@@ -23,40 +23,34 @@ class AuthenticateWithOAuth extends StatefulWidget {
   const AuthenticateWithOAuth({super.key});
 
   @override
-  State<AuthenticateWithOAuth> createState() =>
-      _AuthenticateWithOAuthState();
+  State<AuthenticateWithOAuth> createState() => _AuthenticateWithOAuthState();
 }
 
-class _AuthenticateWithOAuthState
-    extends State<AuthenticateWithOAuth>
+class _AuthenticateWithOAuthState extends State<AuthenticateWithOAuth>
     with SampleStateSupport
     implements ArcGISAuthenticationChallengeHandler {
+  // Create a controller for the map view.
+  final _mapViewController = ArcGISMapView.createController();
+
+  // Create an OAuthUserConfiguration.
   // This document describes the steps to configure OAuth for your app:
-  // https://developers.arcgis.com/documentation/mapping-apis-and-services/security/user-authentication/serverless-native-flow/
+  // https://developers.arcgis.com/documentation/security-and-authentication/user-authentication/flows/authorization-code-with-pkce/
   final _oauthUserConfiguration = OAuthUserConfiguration(
     portalUri: Uri.parse('https://www.arcgis.com'),
     clientId: 'T0A3SudETrIQndd2',
     redirectUri: Uri.parse('my-ags-flutter-app://auth'),
   );
-  final _mapViewController = ArcGISMapView.createController();
 
   @override
   void initState() {
     super.initState();
 
+    // Set this class to the arcGISAuthenticationChallengeHandler property on the authentication manager.
     // This class implements the ArcGISAuthenticationChallengeHandler interface,
     // which allows it to handle authentication challenges via calls to its
     // handleArcGISAuthenticationChallenge() method.
     ArcGISEnvironment
         .authenticationManager.arcGISAuthenticationChallengeHandler = this;
-
-    // A portal item map that has a secure layer (traffic).
-    _mapViewController.arcGISMap = ArcGISMap.withItem(
-      PortalItem.withPortalAndItemId(
-        portal: Portal.arcGISOnline(connection: PortalConnection.authenticated),
-        itemId: 'e5039444ef3c48b8a8fdc9227f9be7c1',
-      ),
-    );
   }
 
   @override
@@ -80,17 +74,30 @@ class _AuthenticateWithOAuthState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Add a map view to the widget tree and set a controller.
       body: ArcGISMapView(
         controllerProvider: () => _mapViewController,
+        onMapViewReady: onMapViewReady,
       ),
     );
+  }
+
+  void onMapViewReady() {
+    // Create a map from a web map that has a secure layer (traffic).
+    final portalItem = PortalItem.withPortalAndItemId(
+      portal: Portal.arcGISOnline(connection: PortalConnection.authenticated),
+      itemId: 'e5039444ef3c48b8a8fdc9227f9be7c1',
+    );
+    final map = ArcGISMap.withItem(portalItem);
+    // Set the map to map view controller.
+    _mapViewController.arcGISMap = map;
   }
 
   @override
   void handleArcGISAuthenticationChallenge(
       ArcGISAuthenticationChallenge challenge) async {
     try {
-      // Initiate the sign in process to the OAuth server.
+      // Initiate the sign in process to the OAuth server using the defined user configuration.
       final credential = await OAuthUserCredential.create(
           configuration: _oauthUserConfiguration);
 
