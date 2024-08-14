ea12a8816faa901e5787c2ebfb9bfb4d564e2bd0
Paul Sturm <psturm@esri.com>
2024-08-14 13:40:46 -0500

    Update to the new package name

--- lib/samples/add_tiled_layer_as_basemap/add_tiled_layer_as_basemap.dart
+++ lib/samples/add_tiled_layer_as_basemap/add_tiled_layer_as_basemap.dart
@@ -14,7 +14,7 @@
 // limitations under the License.
 //
 
-import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:arcgis_maps_sdk/arcgis_maps.dart';
 import 'package:arcgis_maps_sdk_flutter_samples/utils/sample_data.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
