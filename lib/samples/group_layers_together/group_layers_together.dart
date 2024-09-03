bad276a3dcda41c29783b754507aeeeaefbe4bdf
Paul Sturm <psturm@esri.com>
2024-09-03 14:03:38 -0500

    load some layers and build Settings sheet

--- lib/samples/group_layers_together/group_layers_together.dart
+++ lib/samples/group_layers_together/group_layers_together.dart
@@ -13,19 +13,11 @@
 // limitations under the License.
 //
 
+import 'dart:math';
 import 'package:arcgis_maps_sdk/arcgis_maps.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  // Supply your apiKey using the --dart-define-from-file command line argument
-  ArcGISEnvironment.apiKey = const String.fromEnvironment('API_KEY');
-
-  runApp(
-    const MaterialApp(
-      home: GroupLayersTogether(),
-    ),
-  );
-}
+import '../../utils/sample_state_support.dart';
 
 class GroupLayersTogether extends StatefulWidget {
   const GroupLayersTogether({super.key});
@@ -34,11 +26,14 @@ class GroupLayersTogether extends StatefulWidget {
   State<GroupLayersTogether> createState() => _GroupLayersTogetherState();
 }
 
-class _GroupLayersTogetherState extends State<GroupLayersTogether> {
+class _GroupLayersTogetherState extends State<GroupLayersTogether>
+    with SampleStateSupport {
   // Create a controller for the map view.
   final _mapViewController = ArcGISMapView.createController();
   // A flag for when the map view is ready and controls can be used.
   var _ready = false;
+  // A flag for when the settings bottom sheet is visible.
+  var _settingsVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -54,16 +49,15 @@ class _GroupLayersTogetherState extends State<GroupLayersTogether> {
                   child: ArcGISMapView(
                     controllerProvider: () => _mapViewController,
                     onMapViewReady: onMapViewReady,
-                    onTap: onTap,
                   ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    // A button to perform a task.
+                    // A button to show the Settings bottom sheet.
                     ElevatedButton(
-                      onPressed: performTask,
-                      child: const Text('Perform Task'),
+                      onPressed: () => setState(() => _settingsVisible = true),
+                      child: const Text('Settings'),
                     ),
                   ],
                 ),
@@ -82,27 +76,93 @@ class _GroupLayersTogetherState extends State<GroupLayersTogether> {
           ],
         ),
       ),
+      // The Settings bottom sheet.
+      bottomSheet: _settingsVisible ? buildSettings(context) : null,
+    );
+  }
+
+  // The build method for the Settings bottom sheet.
+  Widget buildSettings(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        20.0,
+        0.0,
+        20.0,
+        max(
+          20.0,
+          View.of(context).viewPadding.bottom /
+              View.of(context).devicePixelRatio,
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              Text(
+                'Settings',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const Spacer(),
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () => setState(() => _settingsVisible = false),
+              ),
+            ],
+          ),
+          Container(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.sizeOf(context).height * 0.4,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: _mapViewController.arcGISMap?.operationalLayers
+                        .whereType<GroupLayer>()
+                        .map(buildGroupLayerSettings)
+                        .toList() ??
+                    [],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildGroupLayerSettings(GroupLayer groupLayer) {
+    return ListTile(
+      title: Text(groupLayer.name),
     );
   }
 
   void onMapViewReady() async {
-    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
+    //fixme comments
+    final projectAreaGroupLayer = GroupLayer()..name = 'Project Area Group';
+    final projectAreaTable = ServiceFeatureTable.withUri(
+      Uri.parse(
+        'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/DevelopmentProjectArea/FeatureServer/0',
+      ),
+    );
+    final projectAreaLayer = FeatureLayer.withFeatureTable(projectAreaTable);
+    final pathwaysTable = ServiceFeatureTable.withUri(
+      Uri.parse(
+        'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/DevA_Pathways/FeatureServer/1',
+      ),
+    );
+    final pathwaysLayer = FeatureLayer.withFeatureTable(pathwaysTable);
+    projectAreaGroupLayer.layers.addAll([projectAreaLayer, pathwaysLayer]);
+
+    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISStreets);
+    map.operationalLayers.addAll([projectAreaGroupLayer]);
+
+    await projectAreaLayer.load();
+    if (projectAreaGroupLayer.fullExtent != null) {
+      map.initialViewpoint =
+          Viewpoint.fromTargetExtent(projectAreaGroupLayer.fullExtent!);
+    }
+
     _mapViewController.arcGISMap = map;
-    // Perform some long-running setup task.
-    await Future.delayed(const Duration(seconds: 10));
-    // Set the ready state variable to true to enable the sample UI.
-    setState(() => _ready = true);
-  }
-
-  void onTap(Offset offset) {
-    print('Tapped at $offset');
-  }
-
-  void performTask() async {
-    setState(() => _ready = false);
-    // Perform some task.
-    print('Perform task');
-    await Future.delayed(const Duration(seconds: 5));
     setState(() => _ready = true);
   }
 }
