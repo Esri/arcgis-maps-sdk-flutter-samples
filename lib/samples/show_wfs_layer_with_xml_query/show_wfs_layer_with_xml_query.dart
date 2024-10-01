c0c0b6a2d2888050028cb98ff023ba9b69a14bea
Changan Shi <cshi@esri.com>
2024-10-01 13:42:48 -0700

    update the comments and readme

--- lib/samples/show_wfs_layer_with_xml_query/show_wfs_layer_with_xml_query.dart
+++ lib/samples/show_wfs_layer_with_xml_query/show_wfs_layer_with_xml_query.dart
@@ -14,19 +14,9 @@
 //
 
 import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/utils/sample_state_support.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-void main() async {
-  // Supply your apiKey using the --dart-define-from-file command line argument
-  ArcGISEnvironment.apiKey = const String.fromEnvironment('API_KEY');
-
-  runApp(
-    const MaterialApp(
-      home: ShowWfsLayerWithXmlQuery(),
-    ),
-  );
-}
 
 class ShowWfsLayerWithXmlQuery extends StatefulWidget {
   const ShowWfsLayerWithXmlQuery({super.key});
@@ -36,7 +26,8 @@ class ShowWfsLayerWithXmlQuery extends StatefulWidget {
       _ShowWfsLayerWithXmlQueryState();
 }
 
-class _ShowWfsLayerWithXmlQueryState extends State<ShowWfsLayerWithXmlQuery> {
+class _ShowWfsLayerWithXmlQueryState extends State<ShowWfsLayerWithXmlQuery>
+    with SampleStateSupport {
   // Create a controller for the map view.
   final _mapViewController = ArcGISMapView.createController();
   // A flag for when the map view is ready and controls can be used.
@@ -45,33 +36,30 @@ class _ShowWfsLayerWithXmlQueryState extends State<ShowWfsLayerWithXmlQuery> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        top: false,
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                Expanded(
-                  // Add a map view to the widget tree and set a controller.
-                  child: ArcGISMapView(
-                    controllerProvider: () => _mapViewController,
-                    onMapViewReady: onMapViewReady,
-                  ),
-                ),
-              ],
-            ),
-            // Display a progress indicator and prevent interaction until state is ready.
-            Visibility(
-              visible: !_ready,
-              child: const SizedBox.expand(
-                child: ColoredBox(
-                  color: Colors.white30,
-                  child: Center(child: CircularProgressIndicator()),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                // Add a map view to the widget tree and set a controller.
+                child: ArcGISMapView(
+                  controllerProvider: () => _mapViewController,
+                  onMapViewReady: onMapViewReady,
                 ),
               ),
+            ],
+          ),
+          // Display a progress indicator and prevent interaction until state is ready.
+          Visibility(
+            visible: !_ready,
+            child: const SizedBox.expand(
+              child: ColoredBox(
+                color: Colors.white30,
+                child: Center(child: CircularProgressIndicator()),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -91,32 +79,32 @@ class _ShowWfsLayerWithXmlQueryState extends State<ShowWfsLayerWithXmlQuery> {
     const wfsFeatureTableUri =
         'https://dservices2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/services/Seattle_Downtown_Features/WFSServer?service=wfs&amp;request=getcapabilities';
 
-    // create the WFS feature table from URI and name
+    // Create the WFS feature table from URI and name.
     final statesTable = WfsFeatureTable.withUriAndTableName(
       uri: Uri.parse(wfsFeatureTableUri),
       tableName: 'Seattle_Downtown_Features:Trees',
     )
-      // set the feature request mode and axis order
+      // Set the feature request mode and axis order.
       ..axisOrder = OgcAxisOrder.noSwap
       ..featureRequestMode = FeatureRequestMode.manualCache;
 
-    // create the feature layer from the feature table
+    // Create the feature layer from the feature table.
     final featureLayer = FeatureLayer.withFeatureTable(statesTable);
     await featureLayer.load();
 
-    // add the feature layer to the map
+    // Add the feature layer to the map.
     _mapViewController.arcGISMap?.operationalLayers.add(featureLayer);
 
-    // load the query string from the assets folder
+    // Load the query string from the assets folder.
     final xmlQuery = await rootBundle.loadString('assets/wfs_query.xml');
 
-    // populate the features with the query string
+    // Populate the features with the query string.
     await statesTable.populateFromServiceWithXml(
       xmlRequest: xmlQuery,
       clearCache: true,
     );
 
-    // Zoom to the full extent of the feature layer
+    // Zoom to the full extent of the feature layer.
     _mapViewController.setViewpointGeometry(featureLayer.fullExtent!);
   }
 }
