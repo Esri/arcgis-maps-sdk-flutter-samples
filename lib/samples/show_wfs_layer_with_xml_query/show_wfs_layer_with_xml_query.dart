04064ed67bed9dbe1c4db40d70fb98ccce982bfc
Changan Shi <cshi@esri.com>
2024-10-02 08:26:11 -0700

    adds comments and update sample list

--- lib/samples/show_wfs_layer_with_xml_query/show_wfs_layer_with_xml_query.dart
+++ lib/samples/show_wfs_layer_with_xml_query/show_wfs_layer_with_xml_query.dart
@@ -65,6 +65,7 @@ class _ShowWfsLayerWithXmlQueryState extends State<ShowWfsLayerWithXmlQuery>
   }
 
   void onMapViewReady() async {
+    // Create a map with the ArcGIS Navigation basemap style and set to the map view.
     final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISNavigation);
     _mapViewController.arcGISMap = map;
 
