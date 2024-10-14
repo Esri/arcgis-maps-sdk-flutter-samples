6cef6a319cb393e40db9ef1ee711c1938f57b7a7
martinoyovo <kyovo@esri.com>
2024-10-14 11:11:56 -0700

    ChangeViewpoint sample initial code

---
+++ lib/samples/change_viewpoint/change_viewpoint.dart
@@ -0,0 +1,109 @@
+// Copyright 2024 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  // Supply your apiKey using the --dart-define-from-file command line argument
+  ArcGISEnvironment.apiKey = const String.fromEnvironment('API_KEY');
+
+  runApp(
+    const MaterialApp(
+      home: ChangeViewpoint(),
+    ),
+  );
+}
+
+
+class ChangeViewpoint extends StatefulWidget {
+  const ChangeViewpoint({super.key});
+
+  @override
+  State<ChangeViewpoint> createState() => _ChangeViewpointState();
+}
+
+class _ChangeViewpointState extends State<ChangeViewpoint> {
+  // Create a controller for the map view.
+  final _mapViewController = ArcGISMapView.createController();
+  // A flag for when the map view is ready and controls can be used.
+  var _ready = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  // Add a map view to the widget tree and set a controller.
+                  child: ArcGISMapView(
+                    controllerProvider: () => _mapViewController,
+                    onMapViewReady: onMapViewReady,
+                    onTap: onTap,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // A button to perform a task.
+                    ElevatedButton(
+                      onPressed: performTask,
+                      child: const Text('Perform Task'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            // Display a progress indicator and prevent interaction until state is ready.
+            Visibility(
+              visible: !_ready,
+              child: const SizedBox.expand(
+                child: ColoredBox(
+                  color: Colors.white30,
+                  child: Center(child: CircularProgressIndicator()),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onMapViewReady() async {
+    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
+    _mapViewController.arcGISMap = map;
+    // Perform some long-running setup task.
+    await Future.delayed(const Duration(seconds: 10));
+    // Set the ready state variable to true to enable the sample UI.
+    setState(() => _ready = true);
+  }
+
+  void onTap(Offset offset) {
+    print('Tapped at $offset');
+  }
+
+  void performTask() async {
+    setState(() => _ready = false);
+    // Perform some task.
+    print('Perform task');
+    await Future.delayed(const Duration(seconds: 5));
+    setState(() => _ready = true);
+  }
+}
