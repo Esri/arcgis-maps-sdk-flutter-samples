1c3cd0319df1fd1d3cc34adcad62630cf37795ed
martinoyovo <kyovo@esri.com>
2024-10-14 12:28:03 -0700

    Remove boilerplate code

--- lib/samples/change_viewpoint/change_viewpoint.dart
+++ lib/samples/change_viewpoint/change_viewpoint.dart
@@ -15,17 +15,7 @@
 
 import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:flutter/material.dart';
-
-void main() async {
-  // Supply your apiKey using the --dart-define-from-file command line argument
-  ArcGISEnvironment.apiKey = const String.fromEnvironment('API_KEY');
-
-  runApp(
-    const MaterialApp(
-      home: ChangeViewpoint(),
-    ),
-  );
-}
+import '../../utils/sample_state_support.dart';
 
 class ChangeViewpoint extends StatefulWidget {
   const ChangeViewpoint({super.key});
@@ -34,7 +24,7 @@ class ChangeViewpoint extends StatefulWidget {
   State<ChangeViewpoint> createState() => _ChangeViewpointState();
 }
 
-class _ChangeViewpointState extends State<ChangeViewpoint> {
+class _ChangeViewpointState extends State<ChangeViewpoint> with SampleStateSupport {
   // Create a controller for the map view.
   final _mapViewController = ArcGISMapView.createController();
   // A flag for when the map view is ready and controls can be used.
@@ -54,18 +44,7 @@ class _ChangeViewpointState extends State<ChangeViewpoint> {
                   child: ArcGISMapView(
                     controllerProvider: () => _mapViewController,
                     onMapViewReady: onMapViewReady,
-                    onTap: onTap,
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    // A button to perform a task.
-                    ElevatedButton(
-                      onPressed: performTask,
-                      child: const Text('Perform Task'),
-                    ),
-                  ],
                 ),
               ],
             ),
@@ -172,17 +151,5 @@ class _ChangeViewpointState extends State<ChangeViewpoint> {
 
     // Assign the map to the MapView
     _mapViewController.arcGISMap = map;
-  }
-
-  void onTap(Offset offset) {
-    print('Tapped at $offset');
-  }
-
-  void performTask() async {
-    setState(() => _ready = false);
-    // Perform some task.
-    print('Perform task');
-    await Future.delayed(const Duration(seconds: 5));
-    setState(() => _ready = true);
   }
 }
