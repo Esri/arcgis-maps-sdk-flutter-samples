3da03be8a44d98131bb80a8943a6d97efb997490
Paul Sturm <psturm@esri.com>
2024-11-01 14:21:46 -0500

    Title card and basic map page

--- lib/samples/find_route_in_mobile_map_package/find_route_in_mobile_map_package.dart
+++ lib/samples/find_route_in_mobile_map_package/find_route_in_mobile_map_package.dart
@@ -14,6 +14,7 @@
 //
 
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +33,7 @@ class FindRouteInMobileMapPackage extends StatefulWidget {
 
 class _FindRouteInMobileMapPackageState
     extends State<FindRouteInMobileMapPackage> with SampleStateSupport {
+  //fixme comments throughout
   Future<List<MobileMapPackage>>? mobileMapPackagesFuture;
 
   @override
@@ -47,13 +49,39 @@ class _FindRouteInMobileMapPackageState
         future: mobileMapPackagesFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return Column(
-              children: snapshot.data!.map((mobileMapPackage) {
-                return Text(mobileMapPackage.item?.name ?? '');
-              }).toList(),
+            final maps =
+                snapshot.data?.expand((mmpk) => mmpk.maps).toList() ?? [];
+            return ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              itemCount: maps.length,
+              itemBuilder: (context, index) {
+                final map = maps[index];
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: ListTile(
+                      leading: Image.memory(
+                        map.item?.thumbnail?.image?.getEncodedBuffer() ??
+                            Uint8List(0),
+                      ),
+                      title: Text(map.item?.name ?? ''),
+                      trailing: map.transportationNetworks.isNotEmpty
+                          ? const Icon(Icons.directions_outlined)
+                          : null,
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => FindRouteInMap(map: map),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
             );
           }
-          return const CircularProgressIndicator();
+          return const Center(child: CircularProgressIndicator());
         },
       ),
     );
@@ -71,7 +99,7 @@ class _FindRouteInMobileMapPackageState
 
     final mobileMapPackages = <MobileMapPackage>[];
 
-    for (final filename in ['Yellowstone', 'SanFrancisco']) {
+    for (final filename in ['SanFrancisco', 'Yellowstone']) {
       final mmpkFile = File('${appDir.absolute.path}/$filename.mmpk');
       final mmpk = MobileMapPackage.withFileUri(mmpkFile.uri);
       await mmpk.load();
@@ -79,5 +107,37 @@ class _FindRouteInMobileMapPackageState
     }
 
     return mobileMapPackages;
+  }
+}
+
+class FindRouteInMap extends StatefulWidget {
+  const FindRouteInMap({
+    super.key,
+    required this.map,
+  });
+
+  final ArcGISMap map;
+
+  @override
+  State<FindRouteInMap> createState() => _FindRouteInMapState();
+}
+
+class _FindRouteInMapState extends State<FindRouteInMap> {
+  // Create a controller for the map view.
+  final _mapViewController = ArcGISMapView.createController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.map.item?.name ?? '')),
+      body: ArcGISMapView(
+        controllerProvider: () => _mapViewController,
+        onMapViewReady: onMapViewReady,
+      ),
+    );
+  }
+
+  void onMapViewReady() {
+    _mapViewController.arcGISMap = widget.map;
   }
 }
