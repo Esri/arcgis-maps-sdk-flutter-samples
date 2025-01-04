8aa24f8414a7f754acf5f7037306f32d0cbcc515
Martino (Kossi) Yovo <kyovo@esri.com>
2025-01-03 18:51:06 -0800

    Show network status

--- lib/widgets/sample_detail_page.dart
+++ lib/widgets/sample_detail_page.dart
@@ -1,10 +1,38 @@
+import 'dart:async';
 import 'package:arcgis_maps_sdk_flutter_samples/models/sample.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
-class SampleDetailPage extends StatelessWidget {
+class SampleDetailPage extends StatefulWidget {
   const SampleDetailPage({super.key, required this.sample});
 
   final Sample sample;
+
+  @override
+  State<SampleDetailPage> createState() => _SampleDetailPageState();
+}
+
+class _SampleDetailPageState extends State<SampleDetailPage> {
+  StreamSubscription<List<ConnectivityResult>>? _subscription;
+  var _connected = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _subscription = Connectivity()
+        .onConnectivityChanged
+        .listen((List<ConnectivityResult> result) {
+      setState(() => _connected = !result.contains(ConnectivityResult.none));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_subscription != null) {
+      _subscription!.cancel();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +40,33 @@ class SampleDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: FittedBox(
           fit: BoxFit.scaleDown,
-          child: Text(sample.title),
+          child: Text(widget.sample.title),
         ),
       ),
-      body: sample.getSampleWidget(),
+      body: Stack(
+        children: [
+          widget.sample.getSampleWidget(),
+          if (!_connected)
+            SafeArea(
+              child: IgnorePointer(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  color: Colors.white.withOpacity(0.7),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Network connection is unavailable.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
