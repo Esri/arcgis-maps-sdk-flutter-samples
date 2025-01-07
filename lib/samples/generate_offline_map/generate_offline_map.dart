a6cbc86a5e02d008990fa2a9669b2339e3387c05
Martino (Kossi) Yovo <kyovo@esri.com>
2025-01-06 16:10:17 -0800

    Show dialog when network is unavailable

--- lib/samples/generate_offline_map/generate_offline_map.dart
+++ lib/samples/generate_offline_map/generate_offline_map.dart
@@ -242,15 +242,6 @@ class _GenerateOfflineMapState extends State<GenerateOfflineMap>
       _generateOfflineMapJob = null;
       setState(() => _progress = null);
 
-      // Cancelation:
-      // ArcGISException: code=17; User canceled.; Job canceled.
-
-      // If an error happens (such as cancellation), reset state.
-      setState(() {
-        _generateOfflineMapJob = null;
-        _progress = null;
-      });
-
       // If the exception is not due to user cancellation (code 17), show the details of the error in a dialog.
       if (e.code != 17 && mounted) await showAlertDialog(context, e.message);
       return;
