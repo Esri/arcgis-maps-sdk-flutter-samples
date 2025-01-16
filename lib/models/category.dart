1e627c1722bd3286526aedaba3df6b58d44786d0
Changan Shi <cshi@esri.com>
2025-01-16 08:15:18 -0800

    Cs/Add category UI (#228)
    * refactoring the theme data
    
    * apply the theme data
    
    * move the theme data into common
    
    * reformat the changes
    
    * add the use of icon theme data and apply the color.
    
    * reformat
    
    * remove the redundant style settings.
    
    * remove the icon theme
    
    * update icon theme
    
    * add the category as main UI
    
    * update the UI
    
    * update the space
    
    * update the code with lint
    
    * adds const
    
    * update the application title
    
    * remove some space lines
    
    * minor changes for text string
    
    * rest the original one
    
    * consoliate the category
    
    * Update lib/main.dart
    
    Co-authored-by: Jen Merritt <jmerritt@esri.com>
    
    * Update lib/widgets/sample_viewer_page.dart
    
    Co-authored-by: Jen Merritt <jmerritt@esri.com>
    
    * improve the searchable bar and ui
    
    * run the dart format
    
    * update the title when a category is selected
    
    ---------
    
    Co-authored-by: CHANGAN SHI <>
    Co-authored-by: Jen Merritt <jmerritt@esri.com>

---
+++ lib/models/category.dart
@@ -0,0 +1,71 @@
+//
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import 'package:flutter/material.dart';
+
+enum SampleCategory {
+  all(
+    'All',
+    Icons.apps,
+    'assets/category_images/all_background.webp',
+  ),
+  analysis(
+    'Analysis',
+    Icons.analytics,
+    'assets/category_images/analysis_background.webp',
+  ),
+  cloudAndPortal(
+    'Cloud and Portal',
+    Icons.cloud,
+    'assets/category_images/cloud_background.webp',
+  ),
+  editAndManageData(
+    'Edit and Manage Data',
+    Icons.edit,
+    'assets/category_images/manage_data_background.webp',
+  ),
+  layers(
+    'Layers',
+    Icons.layers,
+    'assets/category_images/layers_background.webp',
+  ),
+  maps(
+    'Maps',
+    Icons.map,
+    'assets/category_images/maps_and_scenes_background.webp',
+  ),
+  routingAndLogistics(
+    'Routing and Logistics',
+    Icons.route,
+    'assets/category_images/routing_and_logistics_background.webp',
+  ),
+  searchAndQuery(
+    'Search and Query',
+    Icons.search,
+    'assets/category_images/search_and_query_background.webp',
+  ),
+  visualization(
+    'Visualization',
+    Icons.visibility,
+    'assets/category_images/visualization_background.webp',
+  ),
+  ;
+
+  const SampleCategory(this.title, this.icon, this.backgroundImage);
+  final String title;
+  final IconData icon;
+  final String backgroundImage;
+}
---
+++ lib/widgets/sample_viewer_page.dart
@@ -0,0 +1,190 @@
+//
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import 'dart:convert';
+import 'package:arcgis_maps_sdk_flutter_samples/models/category.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/models/sample.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/widgets/sample_list_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const applicationTitle = 'ArcGIS Maps SDK for Flutter Samples';
+
+/// A page that displays a list of sample categories.
+class SampleViewerPage extends StatefulWidget {
+  const SampleViewerPage({super.key, this.category, this.isSearchable = true});
+  final SampleCategory? category;
+  final bool isSearchable;
+
+  @override
+  State<SampleViewerPage> createState() => _SampleViewerPageState();
+}
+
+class _SampleViewerPageState extends State<SampleViewerPage> {
+  final _allSamples = <Sample>[];
+  final _searchFocusNode = FocusNode();
+  final _textEditingController = TextEditingController();
+  var _filteredSamples = <Sample>[];
+  var _ready = false;
+  var _searchHasFocus = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loadSamples();
+    _searchFocusNode.addListener(
+      () {
+        if (_searchFocusNode.hasFocus != _searchHasFocus) {
+          setState(() => _searchHasFocus = _searchFocusNode.hasFocus);
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    _searchFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            (widget.category != null && widget.category != SampleCategory.all)
+                ? Text(widget.category!.title)
+                : const Text(applicationTitle),
+      ),
+      body: Column(
+        children: [
+          Visibility(
+            visible: widget.isSearchable,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: TextField(
+                focusNode: _searchFocusNode,
+                controller: _textEditingController,
+                onChanged: onSearchChanged,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  hintText: 'Search...',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _searchHasFocus ? Icons.cancel : Icons.search,
+                    ),
+                    onPressed: onSearchSuffixPressed,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          if (_ready)
+            Expanded(
+              child: Listener(
+                onPointerDown: (_) =>
+                    FocusManager.instance.primaryFocus?.unfocus(),
+                child: SampleListView(samples: _filteredSamples),
+              ),
+            )
+          else
+            const Center(
+              child: Text('Loading samples...'),
+            ),
+        ],
+      ),
+    );
+  }
+
+  void onSearchSuffixPressed() {
+    if (_searchHasFocus) {
+      _textEditingController.clear();
+      FocusManager.instance.primaryFocus?.unfocus();
+      onSearchChanged('');
+    } else {
+      _searchFocusNode.requestFocus();
+    }
+  }
+
+  Future<void> loadSamples() async {
+    final jsonString =
+        await rootBundle.loadString('assets/generated_samples_list.json');
+    final sampleData = jsonDecode(jsonString) as Map<String, dynamic>;
+    for (final s in sampleData.entries) {
+      _allSamples.add(Sample.fromJson(s.value));
+    }
+
+    if (widget.category != null) {
+      setState(() {
+        _filteredSamples = getSamplesByCategory(widget.category);
+      });
+    }
+    setState(() => _ready = true);
+  }
+
+  void onSearchChanged(String searchText) {
+    var results = <Sample>[];
+    // restore the initial list of samples if the search text is empty
+    if (searchText.isEmpty) {
+      if (widget.category == null) {
+        results = [];
+      } else if (widget.category == SampleCategory.all) {
+        results = _allSamples;
+      } else {
+        results = getSamplesByCategory(widget.category);
+      }
+    } else {
+      if (widget.category == null || widget.category == SampleCategory.all) {
+        results = _allSamples.where(
+          (sample) {
+            final lowerSearchText = searchText.toLowerCase();
+            return sample.title.toLowerCase().contains(lowerSearchText) ||
+                sample.category.toLowerCase().contains(lowerSearchText) ||
+                sample.keywords.any(
+                  (keyword) => keyword.toLowerCase().contains(lowerSearchText),
+                );
+          },
+        ).toList();
+        // if the category is not null, the only samples within the category are searched
+      } else {
+        results = getSamplesByCategory(widget.category).where(
+          (sample) {
+            final lowerSearchText = searchText.toLowerCase();
+            return sample.title.toLowerCase().contains(lowerSearchText) ||
+                sample.keywords.any(
+                  (keyword) => keyword.toLowerCase().contains(lowerSearchText),
+                );
+          },
+        ).toList();
+      }
+    }
+    setState(() => _filteredSamples = results);
+  }
+
+  List<Sample> getSamplesByCategory(SampleCategory? category) {
+    if (category == null) {
+      return [];
+    }
+    if (category.title == SampleCategory.all.title) {
+      return _allSamples;
+    }
+
+    return _allSamples.where((sample) {
+      return sample.category.toLowerCase() == category.title.toLowerCase();
+    }).toList();
+  }
+}
