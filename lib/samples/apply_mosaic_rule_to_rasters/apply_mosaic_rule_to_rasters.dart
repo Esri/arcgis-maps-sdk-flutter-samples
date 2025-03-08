85439d008120f7b2797b699598f63cbcae8f6974
CHANGAN SHI <>
2025-03-07 16:46:34 -0800

    init complete the sample

--- lib/samples/apply_mosaic_rule_to_rasters/apply_mosaic_rule_to_rasters.dart
+++ lib/samples/apply_mosaic_rule_to_rasters/apply_mosaic_rule_to_rasters.dart
@@ -21,14 +21,19 @@ class ApplyMosaicRuleToRasters extends StatefulWidget {
   const ApplyMosaicRuleToRasters({super.key});
 
   @override
-  State<ApplyMosaicRuleToRasters> createState() => _ApplyMosaicRuleToRastersState();
+  State<ApplyMosaicRuleToRasters> createState() =>
+      _ApplyMosaicRuleToRastersState();
 }
-
-class _ApplyMosaicRuleToRastersState extends State<ApplyMosaicRuleToRasters> with SampleStateSupport {
+class _ApplyMosaicRuleToRastersState extends State<ApplyMosaicRuleToRasters>
+    with SampleStateSupport {
   // Create a controller for the map view.
   final _mapViewController = ArcGISMapView.createController();
   // A flag for when the map view is ready and controls can be used.
   var _ready = false;
+  
+  var _showMosaicOptions = false;
+  final _mosaicMethods = ['Object ID', 'North West', 'Center', 'By Attribute', 'Lock Raster'];
+  var _selectedMosaicMethod = 'Object ID';
 
   @override
   Widget build(BuildContext context) {
@@ -46,18 +51,7 @@ class _ApplyMosaicRuleToRastersState extends State<ApplyMosaicRuleToRasters> wit
                   child: ArcGISMapView(
                     controllerProvider: () => _mapViewController,
                     onMapViewReady: onMapViewReady,
-                    onTap: onTap,
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    // A button to perform a task.
-                    ElevatedButton(
-                      onPressed: performTask,
-                      child: const Text('Perform Task'),
-                    ),
-                  ],
                 ),
               ],
             ),
@@ -66,6 +60,7 @@ class _ApplyMosaicRuleToRastersState extends State<ApplyMosaicRuleToRasters> wit
           ],
         ),
       ),
+      bottomSheet: _buildBottomSheet(),
     );
   }
 
@@ -73,28 +68,96 @@ class _ApplyMosaicRuleToRastersState extends State<ApplyMosaicRuleToRasters> wit
     // Create a map with a topographic basemap style.
     final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
     _mapViewController.arcGISMap = map;
+    //Create a Raster with the provided URL
+    final raster = ImageServiceRaster(
+      uri: Uri.parse(
+        'https://sampleserver7.arcgisonline.com/server/rest/services/amberg_germany/ImageServer',
+      ),
+    );
 
-    // Perform some long-running setup task.
-    await Future.delayed(const Duration(seconds: 10));
-
+    //Create a RasterLayer with the Raster
+    final rasterLayer = RasterLayer.withRaster(raster);
+    await rasterLayer.load();
+    //Set a default MosaicRule to the RasterLayer
+    raster.mosaicRule = MosaicRule();
+    //Add the RasterLayer to the operational layers of the map
+    map.operationalLayers.add(rasterLayer);
+    await _mapViewController.setViewpointCenter(
+      rasterLayer.fullExtent!.center,
+      scale: 25000,
+    );
     // Set the ready state variable to true to enable the sample UI.
     setState(() => _ready = true);
   }
 
-  void onTap(Offset offset) {
-    // Do something with a tap.
-    // ignore: avoid_print
-    print('Tapped at $offset');
+  Future<void> updateMosaicMethod() async {
+    setState(() => _ready = false);
+    final rasterLayer = _mapViewController
+        .arcGISMap!.operationalLayers.firstWhere((layer) => layer is RasterLayer) as RasterLayer;
+    final raster = rasterLayer.raster! as ImageServiceRaster;
+    switch(_selectedMosaicMethod) {
+      case 'Object ID':
+        raster.mosaicRule = MosaicRule()..mosaicMethod = MosaicMethod.none;
+      case 'North West':
+        raster.mosaicRule = MosaicRule()..mosaicMethod = MosaicMethod.northwest;
+      case 'Center':
+        raster.mosaicRule = MosaicRule()..mosaicMethod = MosaicMethod.center;
+      case 'By Attribute':
+        raster.mosaicRule = MosaicRule()..mosaicMethod = MosaicMethod.attribute;
+      case 'Lock Raster':
+        raster.mosaicRule = MosaicRule()..mosaicMethod = MosaicMethod.lockRaster;
+    } 
+    
+    setState(() => _ready = true);
   }
 
-  Future<void> performTask() async {
-    setState(() => _ready = false);
-
-    // Perform some task.
-    // ignore: avoid_print
-    print('Perform task');
-    await Future.delayed(const Duration(seconds: 5));
-
-    setState(() => _ready = true);
+  Widget _buildBottomSheet() {
+    return BottomSheet(
+      onClosing: () {},
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _showMosaicOptions = !_showMosaicOptions;
+                  });
+                },
+                child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.check, color: Colors.blue),
+                  const SizedBox(width: 8),
+                  Text(_selectedMosaicMethod),
+                ],
+              ),
+              ),
+              if (_showMosaicOptions)
+                ListView(
+                  shrinkWrap: true,
+                  children: _mosaicMethods.map((method) {
+                    return ListTile(
+                      leading: (method == _selectedMosaicMethod)
+                          ? const Icon(Icons.check, color: Colors.blue)
+                          : const SizedBox(width: 16),
+                      title: Text(method),
+                      onTap: () {
+                        setState(() {
+                          _selectedMosaicMethod = method;
+                          _showMosaicOptions = false;
+                        });
+                        updateMosaicMethod();
+                      },
+                    );
+                  }).toList(),
+                ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
