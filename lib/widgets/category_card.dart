4053a20b9bdecfdafe3940bf78d955f53046ef7e
Martino Yovo <martinosoyen@gmail.com>
2025-04-16 03:17:36 -0700

    Category card updated

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,59 +1,129 @@
-//
-// Copyright 2025 Esri
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
-import 'package:arcgis_maps_sdk_flutter_samples/common/common.dart';
 import 'package:arcgis_maps_sdk_flutter_samples/models/category.dart';
 import 'package:flutter/material.dart';
 
-/// A card widget that displays a SampleCategory.
-class CategoryCard extends StatelessWidget {
+class CategoryCard extends StatefulWidget {
   const CategoryCard({
     required this.category,
     required this.onClick,
+    required this.index,
     super.key,
   });
 
   final SampleCategory category;
-  final VoidCallback onClick;
+  final void Function(Offset tapPosition) onClick;
+  final int index;
+
+  @override
+  State<CategoryCard> createState() => _CategoryCardState();
+}
+
+class _CategoryCardState extends State<CategoryCard>
+    with SingleTickerProviderStateMixin {
+  double _scale = 1;
+  bool _iconPressed = false;
+
+  late final AnimationController _controller;
+  late final Animation<Offset> _slideAnimation;
+  late final Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 1.2),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0, 0.6, curve: Curves.easeOut),
+      ),
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.05, 0.5, curve: Curves.easeIn),
+      ),
+    );
+
+    Future.delayed(Duration(milliseconds: widget.index * 120), () {
+      if (mounted) _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      elevation: 4,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: InkWell(
-          onTap: onClick,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              CategoryBackground(category: category),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CategoryIconBackground(category: category),
-                  Text(
-                    category.title,
-                    style: Theme.of(context).textTheme.categoryCardLabelStyle,
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ],
+    return GestureDetector(
+      onTapDown: (details) {
+        setState(() {
+          _scale = 0.95;
+          _iconPressed = true;
+        });
+      },
+      onTapUp: (details) {
+        setState(() {
+          _scale = 1.0;
+          _iconPressed = false;
+        });
+        widget.onClick(details.globalPosition);
+      },
+      onTapCancel:
+          () => setState(() {
+            _scale = 1.0;
+            _iconPressed = false;
+          }),
+      child: AnimatedScale(
+        scale: _scale,
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.easeOut,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          elevation: 4,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                _AnimatedCategoryBackground(category: widget.category),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _AnimatedIcon(
+                      category: widget.category,
+                      pressed: _iconPressed,
+                      index: widget.index,
+                    ),
+                    const SizedBox(height: 8),
+                    FadeTransition(
+                      opacity: _fadeAnimation,
+                      child: SlideTransition(
+                        position: _slideAnimation,
+                        child: Text(
+                          widget.category.title,
+                          style: Theme.of(context).textTheme.titleMedium
+                              ?.copyWith(color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -61,39 +131,104 @@ class CategoryCard extends StatelessWidget {
   }
 }
 
-class CategoryIconBackground extends StatelessWidget {
-  const CategoryIconBackground({required this.category, super.key});
+class _AnimatedIcon extends StatefulWidget {
+  const _AnimatedIcon({
+    required this.category,
+    required this.pressed,
+    required this.index,
+  });
+
   final SampleCategory category;
+  final bool pressed;
+  final int index;
+
+  @override
+  State<_AnimatedIcon> createState() => _AnimatedIconState();
+}
+
+class _AnimatedIconState extends State<_AnimatedIcon>
+    with SingleTickerProviderStateMixin {
+  bool _hovering = false;
+  late final AnimationController _entryController;
+  late final Animation<double> _entryScale;
+
+  @override
+  void initState() {
+    super.initState();
+    _entryController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    _entryScale = Tween<double>(begin: 0.5, end: 1).animate(
+      CurvedAnimation(parent: _entryController, curve: Curves.elasticOut),
+    );
+
+    Future.delayed(Duration(milliseconds: widget.index * 120), () {
+      if (mounted) _entryController.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _entryController.dispose();
+    super.dispose();
+  }
+
+  double get _combinedScale {
+    final hover = _hovering ? 1.15 : 1.0;
+    final press = widget.pressed ? 1.3 : 1.0;
+    return _entryScale.value * hover * press;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.black.withValues(alpha: 0.8),
-        shape: BoxShape.circle,
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hovering = true),
+      onExit: (_) => setState(() => _hovering = false),
+      child: AnimatedBuilder(
+        animation: _entryController,
+        builder: (context, _) {
+          return Transform.scale(
+            scale: _combinedScale,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                color: Colors.black87,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(widget.category.icon, size: 30, color: Colors.white),
+            ),
+          );
+        },
       ),
-      child: Icon(category.icon, size: 30, color: Colors.white),
     );
   }
 }
 
-class CategoryBackground extends StatelessWidget {
-  const CategoryBackground({required this.category, super.key});
+class _AnimatedCategoryBackground extends StatelessWidget {
+  const _AnimatedCategoryBackground({required this.category});
   final SampleCategory category;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.asset(
-          category.backgroundImage,
-          fit: BoxFit.cover,
-          width: double.infinity,
-          height: double.infinity,
-        ),
-        Container(color: Colors.black.withValues(alpha: 0.6)),
-      ],
+    return TweenAnimationBuilder<double>(
+      duration: const Duration(milliseconds: 800),
+      tween: Tween(begin: 1.05, end: 1),
+      curve: Curves.easeOut,
+      builder: (context, scale, child) {
+        return Transform.scale(scale: scale, child: child);
+      },
+      child: Stack(
+        children: [
+          Image.asset(
+            category.backgroundImage,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Container(color: Colors.black.withAlpha(153)),
+        ],
+      ),
     );
   }
 }
