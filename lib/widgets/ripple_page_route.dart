b509389e321fbfc94403f0e048434fb56c564c8e
Martino Yovo <martinosoyen@gmail.com>
2025-04-16 03:33:36 -0700

    Format code

--- lib/widgets/ripple_page_route.dart
+++ lib/widgets/ripple_page_route.dart
@@ -2,35 +2,35 @@ import 'package:flutter/material.dart';
 
 class RipplePageRoute extends PageRouteBuilder {
   RipplePageRoute({required this.position, required this.child})
-      : super(
-    transitionDuration: const Duration(milliseconds: 600),
-    pageBuilder: (_, __, ___) => child,
-    transitionsBuilder: (context, animation, _, child) {
-      final rippleAnim = Tween<double>(
-        begin: 0,
-        end: MediaQuery.of(context).size.longestSide * 2,
-      ).animate(
-        CurvedAnimation(parent: animation, curve: Curves.easeOutCubic),
-      );
+    : super(
+        transitionDuration: const Duration(milliseconds: 600),
+        pageBuilder: (_, __, ___) => child,
+        transitionsBuilder: (context, animation, _, child) {
+          final rippleAnim = Tween<double>(
+            begin: 0,
+            end: MediaQuery.of(context).size.longestSide * 2,
+          ).animate(
+            CurvedAnimation(parent: animation, curve: Curves.easeOutCubic),
+          );
 
-      return AnimatedBuilder(
-        animation: rippleAnim,
-        builder: (context, _) {
-          final size = rippleAnim.value;
+          return AnimatedBuilder(
+            animation: rippleAnim,
+            builder: (context, _) {
+              final size = rippleAnim.value;
 
-          return Stack(
-            children: [
-              // The child page masked by a circular clip.
-              ClipPath(
-                clipper: _RippleClipper(center: position, radius: size / 2),
-                child: child,
-              ),
-            ],
+              return Stack(
+                children: [
+                  // The child page masked by a circular clip.
+                  ClipPath(
+                    clipper: _RippleClipper(center: position, radius: size / 2),
+                    child: child,
+                  ),
+                ],
+              );
+            },
           );
         },
       );
-    },
-  );
   final Offset position;
   final Widget child;
 }
