dbbb01bcd091155a494ba9ea5ad1a0155f5d64ae
Martino Yovo <martinosoyen@gmail.com>
2025-05-05 12:16:53 -0700

    Display scene

---
+++ lib/samples/display_scene/display_scene.dart
@@ -0,0 +1,93 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/common/common.dart';
+import 'package:flutter/material.dart';
+
+class DisplayScene extends StatefulWidget {
+  const DisplayScene({super.key});
+
+  @override
+  State<DisplayScene> createState() => _DisplaySceneState();
+}
+
+class _DisplaySceneState extends State<DisplayScene> with SampleStateSupport {
+  // Create a controller for the scene view.
+  final _sceneViewController = ArcGISSceneView.createController();
+  // A flag for when the map view is ready and controls can be used.
+  var _ready = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          ArcGISSceneView(
+            controllerProvider: () => _sceneViewController,
+            onSceneViewReady: onSceneViewReady,
+          ),
+          // Display a progress indicator and prevent interaction until state is ready.
+          LoadingIndicator(visible: !_ready),
+        ],
+      ),
+    );
+  }
+
+  Future<void> onSceneViewReady() async {
+    // Create a new base map using the static/shared create imagery method.
+    final scene = ArcGISScene.withBasemapStyle(
+      BasemapStyle.arcGISImageryStandard,
+    );
+
+    // Add scene (with an imagery basemap) to the scene view's scene property.
+    _sceneViewController.arcGISScene = scene;
+
+    // Create a new surface.
+    final surface = Surface();
+
+    // Create a Uri from the elevation image service.
+    final myUri = Uri.parse(
+      'https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer',
+    );
+
+    // Create an ArcGIS tiled elevation.
+    final arcGISTiledElevationSource = ArcGISTiledElevationSource.withUri(
+      myUri,
+    );
+
+    // Add the ArcGIS tiled elevation source to the surface's elevated sources collection.
+    surface.elevationSources.add(arcGISTiledElevationSource);
+
+    // Set the scene's base surface to the surface with the ArcGIS tiled elevation source.
+    scene.baseSurface = surface;
+
+    // Create camera with an initial camera position (Mount Everest in the Alps mountains).
+    final camera = Camera.withLatLongAltitudeHeadingPitchRoll(
+      latitude: 45.74,
+      longitude: 6.88,
+      altitude: 4500,
+      heading: 10,
+      pitch: 70,
+      roll: 0,
+    );
+
+    // Set the scene view's camera position.
+    await _sceneViewController.setViewpointCameraAnimated(camera: camera);
+
+    // Set the ready state variable to true to enable the sample UI.
+    setState(() => _ready = true);
+  }
+}
