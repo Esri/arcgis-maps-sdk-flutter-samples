07ff705d5a6ca20cd7955ee9cef3b1a337b73721
Harish Kumar Reddy Kunchala <hkunchala@esri.com>
2025-05-22 11:02:55 -0700

    Select Features in Scene Layer Sample

---
+++ lib/samples/select_features_in_scene_layer/README.md
@@ -0,0 +1,35 @@
+# Select features in scene layer
+
+Identify features in a scene to select.
+
+![Image of select features in scene layer](select_features_in_scene_layer.png)
+
+## Use case
+
+You can select features to visually distinguish them with a selection color or highlighting. This can be useful to demonstrate the physical extent or associated attributes of a feature, or to initiate another action such as centering that feature in the scene view.
+
+## How to use the sample
+
+Click on a building in the scene layer to select it. Deselect buildings by clicking away from the buildings.
+
+## How it works
+
+1. Create an `ArcGISSceneLayer` passing in the URL to a scene layer service.
+2. Use `onTap` callback of the `ArcGISSceneView` to get the screen tap location `screenPoint`.
+3. Call `ArcGISSceneViewController.identifyLayer(sceneLayer, screenPoint, tolerance, false, 1)` to identify features in the scene.
+4. From the resulting `IdentifyLayerResult`, get the list of identified `GeoElements` with `result.geoElements`.
+5. Get the first element in the list, checking that it is a feature, and call `sceneLayer.selectFeature(feature)` to select it.
+
+## Relevant API
+
+* ArcGISSceneLayer
+* Scene
+* SceneView
+
+## About the data
+
+This sample shows a [Berlin, Germany Scene](https://www.arcgis.com/home/item.html?id=31874da8a16d45bfbc1273422f772270) hosted on ArcGIS Online.
+
+## Tags
+
+3D, Berlin, buildings, identify, model, query, search, select
---
+++ lib/samples/select_features_in_scene_layer/select_features_in_scene_layer.dart
@@ -0,0 +1,123 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/common/common.dart';
+import 'package:flutter/material.dart';
+
+class SelectFeaturesInSceneLayer extends StatefulWidget {
+  const SelectFeaturesInSceneLayer({super.key});
+
+  @override
+  State<SelectFeaturesInSceneLayer> createState() =>
+      _SelectFeaturesInSceneLayerState();
+}
+
+class _SelectFeaturesInSceneLayerState extends State<SelectFeaturesInSceneLayer>
+    with SampleStateSupport {
+  // Create a controller for the scene view.
+  final _sceneViewController = ArcGISSceneView.createController();
+
+  // Define a ArcGISSceneLayer to get from the URI
+  late ArcGISSceneLayer _sceneLayer;
+
+  // A flag for when the map view is ready and controls can be used.
+  var _ready = false;
+
+  // A flag to prevent multiple simultaneous identify operations.
+  var _isIdentifying = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        left: false,
+        right: false,
+        child: Stack(
+          children: [
+            ArcGISSceneView(
+              controllerProvider: () => _sceneViewController,
+              onSceneViewReady: onSceneViewReady,
+              onTap: onTap,
+            ),
+            // Display a progress indicator and prevent interaction until state is ready.
+            LoadingIndicator(visible: !_ready),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> onSceneViewReady() async {
+    // Create a scene with from the uri.
+    final scene = ArcGISScene.withUri(
+      Uri.parse(
+        'https://www.arcgis.com/home/item.html?id=31874da8a16d45bfbc1273422f772270',
+      ),
+    );
+    // Load the scene.
+    await scene!.load();
+    // Set the scene to ArcGISSceneViewController
+    _sceneViewController.arcGISScene = scene;
+
+    // Get the operational layers from ArcGISScene
+    final operationalLayers = scene.operationalLayers;
+    // There are two layers from the provided Berlin, Germany Scene. We need the ArcGISSceneLayer
+    _sceneLayer =
+        operationalLayers.firstWhere(
+              (layer) => layer.runtimeType == ArcGISSceneLayer,
+            )
+            as ArcGISSceneLayer;
+
+    // Set the ready state variable to true to enable the sample UI.
+    setState(() => _ready = true);
+  }
+
+  Future<void> onTap(Offset offset) async {
+    // If an identify operation is already in progress, ignore this tap.
+    if (_isIdentifying) return;
+
+    // Set the flag to indicate an identify operation is in progress.
+    _isIdentifying = true;
+
+    try {
+      // Clear any previously selected features.
+      _sceneLayer.clearSelection();
+
+      // Identify features at the tapped screen location.
+      final identifyLayerResult = await _sceneViewController.identifyLayer(
+        _sceneLayer,
+        screenPoint: offset,
+        tolerance: 22,
+      );
+
+      // Filter the identified GeoElements to only include features.
+      final features =
+          identifyLayerResult.geoElements.whereType<Feature>().toList();
+
+      // If no features were identified, clear any existing selection.
+      if (features.isEmpty) {
+        _sceneLayer.clearSelection();
+      } else {
+        // Otherwise, select the identified features to highlight them.
+        _sceneLayer.selectFeatures(features);
+      }
+    } finally {
+      // Reset the flag to allow future identify operations.
+      _isIdentifying = false;
+    }
+  }
+}
