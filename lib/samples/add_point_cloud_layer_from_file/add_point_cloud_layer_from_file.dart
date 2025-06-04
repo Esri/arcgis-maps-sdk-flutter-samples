ec45cc5486329943cd2e7245b496a8d4d5e5efd3
Martino Yovo <martinosoyen@gmail.com>
2025-06-03 17:40:49 -0700

    Martinoyovo/add point cloud layer from file (#300)
    * Add point cloud layer from file sample
    
    * Add point cloud layer from file sample
    
    * Add correct sample image path
    
    * Update lib/samples/add_point_cloud_layer_from_file/add_point_cloud_layer_from_file.dart
    
    Co-authored-by: Jen Merritt <jmerritt@esri.com>
    
    * Update lib/samples/add_point_cloud_layer_from_file/add_point_cloud_layer_from_file.dart
    
    Co-authored-by: Jen Merritt <jmerritt@esri.com>
    
    * Update camera initialization comment
    
    * Refactor code.
    
    ---------
    
    Co-authored-by: Jen Merritt <jmerritt@esri.com>

---
+++ lib/samples/add_point_cloud_layer_from_file/add_point_cloud_layer_from_file.dart
@@ -0,0 +1,118 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import 'dart:io';
+
+import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/common/common.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class AddPointCloudLayerFromFile extends StatefulWidget {
+  const AddPointCloudLayerFromFile({super.key});
+
+  @override
+  State<AddPointCloudLayerFromFile> createState() =>
+      _AddPointCloudLayerFromFileState();
+}
+
+class _AddPointCloudLayerFromFileState extends State<AddPointCloudLayerFromFile>
+    with SampleStateSupport {
+  // Create a controller for the scene view.
+  final _sceneViewController = ArcGISSceneView.createController();
+
+  // A flag for when the scene view is ready.
+  var _ready = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          ArcGISSceneView(
+            controllerProvider: () => _sceneViewController,
+            onSceneViewReady: onSceneViewReady,
+          ),
+          // Display a progress indicator and prevent interaction until state is ready.
+          LoadingIndicator(visible: !_ready),
+        ],
+      ),
+    );
+  }
+
+  Future<void> onSceneViewReady() async {
+    // Add the scene to the view controller.
+    final scene = _setupScene();
+    _sceneViewController.arcGISScene = scene;
+
+    // Load the point cloud layer.
+    final pointCloudLayer = await loadCloudPointLayerFromFile();
+
+    // Add the point cloud layer to the map's operational layers.
+    scene.operationalLayers.add(pointCloudLayer);
+
+    // Add the scene to the scene view's scene property.
+    _sceneViewController.arcGISScene = scene;
+
+    // Set the ready state variable to true to enable the sample UI.
+    setState(() => _ready = true);
+  }
+
+  ArcGISScene _setupScene() {
+    // Create a scene with an imagery basemap style.
+    final scene = ArcGISScene.withBasemapStyle(BasemapStyle.arcGISImagery);
+
+    // Set the scene's initial viewpoint.
+    scene.initialViewpoint = Viewpoint.withPointScaleCamera(
+      center: ArcGISPoint(x: 0, y: 0),
+      scale: 1,
+      camera: Camera.withLatLong(
+        latitude: 32.720195,
+        longitude: -117.155593,
+        altitude: 1050,
+        heading: 23,
+        pitch: 70,
+        roll: 0,
+      ),
+    );
+
+    // Add surface elevation to the scene.
+    final surface = Surface();
+    final worldElevationService = Uri.parse(
+      'https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer',
+    );
+    final elevationSource = ArcGISTiledElevationSource.withUri(
+      worldElevationService,
+    );
+    surface.elevationSources.add(elevationSource);
+    scene.baseSurface = surface;
+
+    return scene;
+  }
+
+  Future<PointCloudLayer> loadCloudPointLayerFromFile() async {
+    // Download the sample data.
+    await downloadSampleData(['34da965ca51d4c68aa9b3a38edb29e00']);
+    // Get the temp directory.
+    final directory = await getApplicationDocumentsDirectory();
+    // Create a file reference to the scene layer package for Balboa Park, San Diego, CA.
+    final sanDiegoPointCloudFile = File(
+      '${directory.absolute.path}/sandiego-north-balboa-pointcloud.slpk',
+    );
+    // Create a Point Cloud Layer from the file URI.
+    final pointCloud = PointCloudLayer.withUri(sanDiegoPointCloudFile.uri);
+    return pointCloud;
+  }
+}
