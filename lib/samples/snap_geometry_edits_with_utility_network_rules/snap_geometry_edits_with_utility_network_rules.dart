95386be1236ce4f5ccf762534920bc45ab46b52a
Paul Sturm <psturm@esri.com>
2025-09-04 16:40:24 -0500

    start loading data

--- lib/samples/snap_geometry_edits_with_utility_network_rules/snap_geometry_edits_with_utility_network_rules.dart
+++ lib/samples/snap_geometry_edits_with_utility_network_rules/snap_geometry_edits_with_utility_network_rules.dart
@@ -13,18 +13,24 @@
 // limitations under the License.
 //
 
+import 'dart:io';
+
 import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:arcgis_maps_sdk_flutter_samples/common/common.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class SnapGeometryEditsWithUtilityNetworkRules extends StatefulWidget {
   const SnapGeometryEditsWithUtilityNetworkRules({super.key});
 
   @override
-  State<SnapGeometryEditsWithUtilityNetworkRules> createState() => _SnapGeometryEditsWithUtilityNetworkRulesState();
+  State<SnapGeometryEditsWithUtilityNetworkRules> createState() =>
+      _SnapGeometryEditsWithUtilityNetworkRulesState();
 }
 
-class _SnapGeometryEditsWithUtilityNetworkRulesState extends State<SnapGeometryEditsWithUtilityNetworkRules> with SampleStateSupport {
+class _SnapGeometryEditsWithUtilityNetworkRulesState
+    extends State<SnapGeometryEditsWithUtilityNetworkRules>
+    with SampleStateSupport {
   // Create a controller for the map view.
   final _mapViewController = ArcGISMapView.createController();
   // A flag for when the map view is ready and controls can be used.
@@ -69,13 +75,43 @@ class _SnapGeometryEditsWithUtilityNetworkRulesState extends State<SnapGeometryE
     );
   }
 
+  //fixme comments
   Future<void> onMapViewReady() async {
-    // Create a map with a topographic basemap style.
-    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
+    const downloadFileName = 'NapervilleGasUtilities';
+    final appDir = await getApplicationDocumentsDirectory();
+    final zipFile = File('${appDir.absolute.path}/$downloadFileName.zip');
+    if (!zipFile.existsSync()) {
+      await downloadSampleDataWithProgress(
+        itemIds: ['0fd3a39660d54c12b05d5f81f207dffd'],
+        destinationFiles: [zipFile],
+      );
+    }
+    final geodatabaseFile = File(
+      '${appDir.absolute.path}/$downloadFileName/$downloadFileName.geodatabase',
+    );
+
+    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISStreetsNight);
+    map.initialViewpoint = Viewpoint.fromCenter(
+      ArcGISPoint(
+        x: -9811055.1560284,
+        y: 5131792.195025,
+        spatialReference: SpatialReference.webMercator,
+      ),
+      scale: 10000,
+    );
+    map.loadSettings.featureTilingMode =
+        FeatureTilingMode.enabledWithFullResolutionWhenSupported;
     _mapViewController.arcGISMap = map;
 
-    // Perform some long-running setup task.
-    await Future<void>.delayed(const Duration(seconds: 10));
+    final geodatabase = Geodatabase.withFileUri(geodatabaseFile.uri);
+    await geodatabase.load();
+    final pipelineLayer = SubtypeFeatureLayer.withFeatureTable(
+      geodatabase.getGeodatabaseFeatureTable(tableName: 'PipelineLine')!
+          as ArcGISFeatureTable,
+    );
+    map.operationalLayers.add(pipelineLayer);
+
+    //fixme
 
     // Set the ready state variable to true to enable the sample UI.
     setState(() => _ready = true);
