10fa67c5494d04079c968e173fc94c5aeefbd508
Kevin Mueller <kmueller@esri.com>
2026-02-06 15:43:50 -0600

    Added new sample

---
+++ lib/samples/modify_local_scene_environment_settings/README.md
@@ -0,0 +1,64 @@
+# Modify local scene environment settings
+
+Modify the environment settings in a local scene to change the lighting conditions and background appearance.
+
+![Image of Modify Local Scene Environment Settings](ModifyLocalSceneEnvironmentSettings.png)
+
+## Use case
+
+Environment settings for a LocalSceneView define how the background or sky appears, if objects in the scene cast shadows, and if virtual lighting or simulated sunlight is used to light the scene.
+
+By activating sun lighting, a user can visualize how shadows are cast throughout the day. If the atmosphere and stars are active, the blue sky will show during daylight hours, and a realistic star field will show at night.
+
+Alternatively, a scene may be rendered using a virtual light source, with shadows disabled, and with a simple solid background color in place of the sky. The virtual light source is slightly offset from the camera instead of a time-based location.
+
+## How to use the sample
+
+At start-up, you will see a local scene with a set of scene environment controls at the bottom. Adjusting the controls will change how the scene is rendered in real time.
+
+### Background settings
+
+Toggling the “Stars” and “Atmosphere” buttons will enable or disable those features. Selecting a color from the dropdown will set a solid color for the background color.
+
+Some notes about the behavior of the sky and background:
+
+* The atmosphere is rendered in front of the stars and the stars are in front of the background color.
+* Stars are not rendered when using virtual lighting.
+* To fully see the background color, atmosphere and stars must be deactivated.
+* The background color shows in the night sky if the atmosphere is enabled and the stars are disabled.
+
+### Lighting settings
+
+The lighting buttons switch between sun lighting and virtual lighting. The “Direct Shadows” button will enable or disable the rendering of shadows for 3d objects in the scene. Shadows are not rendered for surface terrain. If sun lighting is active, the slider under the buttons will set the hour of the day ranging from midnight to 11pm (23:00). Dragging the bar will change the position of the simulated Sun causing changes to shading and direct shadows.
+
+## How it works
+
+When the sample is started, a `ArcGISScene` is loaded from an online resource and applied to the `ArcGISLocalSceneViewController.arcGISScene` property. The sample’s controls are updated to reflect the current state of the scene’s environment property.
+
+Changes to the settings in the background controls set values directly on the `SceneEnvironment` object.
+
+* `isAtmosphereEnabled` and `areStarsEnabled` are boolean properties dictating whether the atmosphere and star field are visible.
+* Colors selected from the dropdown are set to the `backgroundColor`.
+
+Changes to the settings in the lighting controls manipulate the `SceneLighting` object in the `SceneEnvironment.lighting` property.
+
+* Switching between “Sun” and “Virtual” lighting assigns a new `SunLighting` or `VirtualLighting` object to the lighting property.
+* The “Direct Shadows” button sets the `areDirectShadowsEnabled` boolean property on the lightning object. This toggles the shadows cast by object in the scene.
+* If `SunLighting` is active, manipulating the slider changes the hour of the `simulatedDate` property on the lighting object. `VirtualLighting` does not have a date time property so the slider is disabled.
+
+## Relevant API
+
+* ArcGISLocalSceneView
+* ArcGISScene
+* SceneEnvironment
+* SceneLighting
+* SunLighting
+* VirtualLighting
+
+## About the data
+
+The [web scene](https://maps.arcgis.com/home/item.html?id=fcebd77958634ac3874bbc0e6b0677a4) used for this sample contains a clipped local scene consisting of a surface and 3D objects representing the buildings. The scene is located in Santa Fe, New Mexico, USA.
+
+## Tags
+
+3D, environment, lighting, scene
---
+++ lib/samples/modify_local_scene_environment_settings/modify_local_scene_environment_settings.dart
@@ -0,0 +1,352 @@
+// Copyright 2026 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/common/common.dart';
+import 'package:flutter/material.dart';
+
+class ModifyLocalSceneEnvironmentSettings extends StatefulWidget {
+  const ModifyLocalSceneEnvironmentSettings({super.key});
+
+  @override
+  State<ModifyLocalSceneEnvironmentSettings> createState() =>
+      _ModifyLocalSceneEnvironmentSettingsState();
+}
+
+class _ModifyLocalSceneEnvironmentSettingsState
+    extends State<ModifyLocalSceneEnvironmentSettings>
+    with SampleStateSupport {
+  // Get a controller for the ArcGISLocalSceneView
+  final _localSceneViewController = ArcGISLocalSceneView.createController();
+
+  // Listing used for background color drop down.
+  final _backgroundColorOptions = [
+    (name: 'Black', color: Colors.black),
+    (name: 'Red', color: Colors.red),
+    (name: 'Orange', color: Colors.orange),
+    (name: 'Yellow', color: Colors.yellow),
+    (name: 'Green', color: Colors.green),
+    (name: 'Blue', color: Colors.blue),
+    (name: 'Purple', color: Colors.purple),
+    (name: 'White', color: Colors.white),
+  ];
+
+  // Initialize state variables for the control panel.
+  var _isStarsEnabled = true;
+  var _isAtmosphereEnabled = true;
+  var _isDirectShadowsEnabled = false;
+  var _backgroundColor = Colors.black;
+  var _isSunLighting = false;
+  var _lightingDateTime = DateTime.utc(2026, 3, 20, 12);
+  var _lightingTimeZoneOffset = Duration.zero;
+  var _lightingHour = 12;
+
+  // A flag for when the map view is ready and controls can be used.
+  var _ready = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        left: false,
+        right: false,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  child: ArcGISLocalSceneView(
+                    controllerProvider: () => _localSceneViewController,
+                    onLocalSceneViewReady: onLocalSceneReady,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Scene Environment Settings:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const Divider(),
+                      Row(
+                        children: [
+                          const Spacer(),
+                          const Text('Background:'),
+                          const Spacer(),
+                          ToggleButtons(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                            isSelected: [_isStarsEnabled, _isAtmosphereEnabled],
+                            onPressed: (index) {
+                              switch (index) {
+                                case 0:
+                                  changeEnableStars(!_isStarsEnabled);
+                                case 1:
+                                  changeEnableAtmosphere(!_isAtmosphereEnabled);
+                              }
+                            },
+                            children: const [
+                              Padding(
+                                padding: EdgeInsetsGeometry.fromLTRB(
+                                  10,
+                                  0,
+                                  10,
+                                  0,
+                                ),
+                                child: Text('Stars'),
+                              ),
+                              Padding(
+                                padding: EdgeInsetsGeometry.fromLTRB(
+                                  10,
+                                  0,
+                                  10,
+                                  0,
+                                ),
+                                child: Text('Atmosphere'),
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          DropdownButton(
+                            value: _backgroundColor,
+                            items: _backgroundColorOptions
+                                .map<DropdownMenuItem<Color>>(
+                                  (colorOption) => DropdownMenuItem(
+                                    value: colorOption.color,
+                                    child: Text(colorOption.name),
+                                  ),
+                                )
+                                .toList(),
+                            onChanged: changeBackgroundColor,
+                          ),
+                          const Spacer(),
+                        ],
+                      ),
+                      const Divider(),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Spacer(),
+                              const Text('Lighting:'),
+                              const Spacer(),
+                              ToggleButtons(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(8),
+                                ),
+                                isSelected: [_isSunLighting, !_isSunLighting],
+                                onPressed: (index) =>
+                                    changeLightingType(index == 0),
+                                children: const [
+                                  Padding(
+                                    padding: EdgeInsetsGeometry.fromLTRB(
+                                      10,
+                                      0,
+                                      10,
+                                      0,
+                                    ),
+                                    child: Text('Sun'),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsetsGeometry.fromLTRB(
+                                      10,
+                                      0,
+                                      10,
+                                      0,
+                                    ),
+                                    child: Text('Virtual'),
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              ToggleButtons(
+                                isSelected: [_isDirectShadowsEnabled],
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(8),
+                                ),
+                                onPressed: (_) => changeEnableShadows(
+                                  !_isDirectShadowsEnabled,
+                                ),
+                                children: const [
+                                  Padding(
+                                    padding: EdgeInsetsGeometry.fromLTRB(
+                                      10,
+                                      0,
+                                      10,
+                                      0,
+                                    ),
+                                    child: Text('Direct Shadows'),
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                            ],
+                          ),
+                          Slider(
+                            value: _lightingHour.toDouble(),
+                            max: 23,
+                            divisions: 24,
+                            label: '$_lightingHour:00',
+                            onChanged: _isSunLighting
+                                ? updateLightingHour
+                                : null,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            // Display a progress indicator and prevent interaction until state is ready.
+            LoadingIndicator(visible: !_ready),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> onLocalSceneReady() async {
+    // Create and load the web scene
+    final websceneUri = Uri.parse(
+      'https://maps.arcgis.com/home/item.html?id=fcebd77958634ac3874bbc0e6b0677a4',
+    ); // Local scene with 3D trees and buildings
+    final scene = ArcGISScene.withUri(websceneUri)!;
+    await scene.load();
+
+    // Set the scene on the local scene view.
+    _localSceneViewController.arcGISScene = scene;
+
+    // Set the state variables based on the actual scene environment.
+    setState(() {
+      _isAtmosphereEnabled = scene.environment.isAtmosphereEnabled;
+      _isStarsEnabled = scene.environment.areStarsEnabled;
+      _isDirectShadowsEnabled =
+          scene.environment.lighting.areDirectShadowsEnabled;
+      _backgroundColor = scene.environment.backgroundColor;
+
+      if (scene.environment.lighting is SunLighting) {
+        // Record the simulated time from the web scene.
+        final sunLighting = scene.environment.lighting as SunLighting;
+        _isSunLighting = true;
+
+        _lightingDateTime = sunLighting.simulatedDate;
+
+        // Record the time zone offset if one was set on the web scene.
+        if (sunLighting.displayTimeZone != null) {
+          _lightingTimeZoneOffset = Duration(
+            hours: sunLighting.displayTimeZone!.hours,
+            minutes: sunLighting.displayTimeZone!.minutes,
+          );
+        }
+
+        _lightingHour = sunLighting.simulatedDate
+            .add(_lightingTimeZoneOffset)
+            .hour;
+      }
+
+      // The view is ready for interaction.
+      _ready = true;
+    });
+  }
+
+  /// Function that handles a change in the stars flag.
+  void changeEnableStars(bool enabled) {
+    _localSceneViewController.arcGISScene!.environment.areStarsEnabled =
+        enabled;
+    setState(() => _isStarsEnabled = enabled);
+  }
+
+  /// Function that handles a change in the atmoshpere flag.
+  void changeEnableAtmosphere(bool enabled) {
+    _localSceneViewController.arcGISScene!.environment.isAtmosphereEnabled =
+        enabled;
+    setState(() => _isAtmosphereEnabled = enabled);
+  }
+
+  /// Function that handles a change in the background color.
+  void changeBackgroundColor(Color? backgroundColor) {
+    final newColor = backgroundColor ?? Colors.black;
+    _localSceneViewController.arcGISScene!.environment.backgroundColor =
+        newColor;
+    setState(() {
+      _backgroundColor = newColor;
+    });
+  }
+
+  /// Function that handles a change in the direct shadows flag.
+  void changeEnableShadows(bool enabled) {
+    _localSceneViewController
+            .arcGISScene!
+            .environment
+            .lighting
+            .areDirectShadowsEnabled =
+        enabled;
+    setState(() => _isDirectShadowsEnabled = enabled);
+  }
+
+  /// Function to change the scene lighting type.
+  void changeLightingType(bool isSunLighting) {
+    // Build the new scene lighting object.
+    final SceneLighting newSceneLighting;
+    if (isSunLighting) {
+      newSceneLighting = SunLighting(
+        simulatedDate: _lightingDateTime,
+        areDirectShadowsEnabled: _isDirectShadowsEnabled,
+      );
+    } else {
+      newSceneLighting = VirtualLighting(
+        areDirectShadowsEnabled: _isDirectShadowsEnabled,
+      );
+    }
+
+    // Set the new lighting object to the scene.
+    _localSceneViewController.arcGISScene?.environment.lighting =
+        newSceneLighting;
+
+    setState(() {
+      // Set the lighting type button state.
+      _isSunLighting = isSunLighting;
+
+      if (_isSunLighting) {
+        // Ensure the slider is showing the correct hour.
+        _lightingHour = _lightingDateTime.add(_lightingTimeZoneOffset).hour;
+      }
+    });
+  }
+
+  /// Function to handle the change in the lighting hour.
+  void updateLightingHour(double newHourValue) {
+    if (!_isSunLighting) return;
+
+    final newHourInt = newHourValue.round();
+
+    final hourDif = newHourInt - _lightingHour;
+    setState(() => _lightingHour = newHourInt);
+
+    final hourChangeDuration = Duration(hours: hourDif);
+    _lightingDateTime = _lightingDateTime.add(hourChangeDuration);
+
+    (_localSceneViewController.arcGISScene!.environment.lighting as SunLighting)
+            .simulatedDate =
+        _lightingDateTime;
+  }
+}
