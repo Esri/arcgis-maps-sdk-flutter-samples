d62427757a55267b16cc0527efbd715abe65353a
Changan Shi <cshi@esri.com>
2026-05-11 14:53:38 -0700

    progress save

---
+++ lib/samples/search_symbol_style_dictionary/README.md
@@ -0,0 +1,38 @@
+# Search symbol style dictionary
+
+Find symbols within the mil2525d specification that match a keyword.
+
+![Image of search symbol style dictionary](search_symbol_style_dictionary.png)
+
+## Use case
+
+You can use support for military symbology to allow users to report changes in the field using the correct military symbols.
+
+## How to use the sample
+
+By default, leaving the fields blank and hitting search will find all symbols.
+
+To search for certain symbols, enter text into one or more search boxes and click 'Search for symbols'. Results are shown in a list. Pressing 'Clear' will reset the search.
+
+## How it works
+
+1. Create a symbol dictionary with the mil2525d specification by passing the string "mil2525d" and the path to a .stylx file to the `SymbolDictionary` constructor.
+2. Create `StyleSymbolSearchParameters`.
+3. Add members to the `names`, `tags`, `symbolClasses`, `categories`, and `keys` list fields of the search parameters.
+4. Search for symbols using the parameters with `symbolDictionary.searchSymbolsAsync(styleSymbolSearchParameters)`.
+5. Get the `Symbol` from the list of returned `StyleSymbolSearchResult`s.
+
+## Relevant API
+
+* Symbol
+* SymbolDictionary
+* SymbolStyleSearchParameters
+* SymbolStyleSearchResult
+
+## Additional information
+
+This sample features the mil2525D specification. ArcGIS Maps SDK supports other military symbology standards, including mil2525C and mil2525B(change 2). See the [Military Symbology Styles](https://solutions.arcgis.com/defense/help/military-symbology-styles/) overview on *ArcGIS Solutions for Defense* for more information about support for military symbology.
+
+## Tags
+
+CIM, defense, look up, MIL-STD-2525B, MIL-STD-2525C, MIL-STD-2525D, mil2525b, mil2525c, mil2525d, military, military symbology, search, symbology
---
+++ lib/samples/search_symbol_style_dictionary/search_symbol_style_dictionary.dart
@@ -0,0 +1,465 @@
+// Copyright 2026 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import 'dart:io';
+
+import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/common/common.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SearchSymbolStyleDictionary extends StatefulWidget {
+  const SearchSymbolStyleDictionary({super.key});
+
+  @override
+  State<SearchSymbolStyleDictionary> createState() =>
+      _SearchSymbolStyleDictionaryState();
+}
+
+class _SearchSymbolStyleDictionaryState
+    extends State<SearchSymbolStyleDictionary>
+    with SampleStateSupport {
+  final _nameController = TextEditingController(text: 'Maritime');
+  final _tagController = TextEditingController();
+  final _symbolClassController = TextEditingController();
+  final _categoryController = TextEditingController();
+  final _keyController = TextEditingController();
+
+  var _ready = true;
+  var _resultCount = 0;
+  final _results = <_SymbolSearchPreview>[];
+
+  SymbolStyle? _symbolStyle;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _tagController.dispose();
+    _symbolClassController.dispose();
+    _categoryController.dispose();
+    _keyController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 800),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 20, 16, 16),
+                  child: Column(
+                    spacing: 8,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        'Find symbols in the mil2525d specification using one or more search filters.',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      _SearchFields(
+                        nameController: _nameController,
+                        tagController: _tagController,
+                        symbolClassController: _symbolClassController,
+                        categoryController: _categoryController,
+                        keyController: _keyController,
+                      ),
+                      Wrap(
+                        spacing: 12,
+                        runSpacing: 8,
+                        children: [
+                          FilledButton.icon(
+                            onPressed: _performSearch,
+                            icon: const Icon(Icons.search),
+                            label: const Text('Search for symbols'),
+                          ),
+                          OutlinedButton.icon(
+                            onPressed: _clear,
+                            icon: const Icon(Icons.clear),
+                            label: const Text('Clear'),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 14),
+                      Text(
+                        'Results found: $_resultCount',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      const SizedBox(height: 10),
+                      Expanded(
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Theme.of(
+                              context,
+                            ).colorScheme.surfaceContainerLow,
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Theme.of(
+                                context,
+                              ).colorScheme.outlineVariant,
+                            ),
+                          ),
+                          child: _results.isEmpty
+                              ? const Center(
+                                  child: Text(
+                                    'No symbols to display. Run a search to see results.',
+                                  ),
+                                )
+                              : ListView.separated(
+                                  padding: const EdgeInsets.all(12),
+                                  itemCount: _results.length,
+                                  separatorBuilder: (_, _) =>
+                                      const SizedBox(height: 8),
+                                  itemBuilder: (context, index) {
+                                    final result = _results[index];
+                                    return _SearchResultCard(result: result);
+                                  },
+                                ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            LoadingIndicator(visible: !_ready),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _performSearch() async {
+    if (_symbolStyle == null) {
+      // Download the sample data.
+      final listPaths = GoRouter.of(context).state.extra! as List<String>;
+      final stylxPath = listPaths.first;
+      if (listPaths.isNotEmpty) {
+        final file = File(stylxPath);
+        if (!file.existsSync() || file.lengthSync() == 0) {
+          throw Exception(
+            'Symbol style dictionary file not found or empty at path: ${file.path}',
+          );
+        }
+      }
+
+      _symbolStyle = SymbolStyle.withStyleLocation(stylxPath);
+      await _symbolStyle!.load();
+      if (_symbolStyle!.loadStatus != LoadStatus.loaded) {
+        throw Exception(
+          'Failed to load the symbol style dictionary: ${_symbolStyle!.loadError}',
+        );
+      }
+    }
+    // get parameters from input fields
+    final searchFilter = SymbolStyleSearchParameters();
+    searchFilter.names.add(_nameController.text);
+    searchFilter.tags.add(_tagController.text);
+    searchFilter.symbolClasses.add(_symbolClassController.text);
+    searchFilter.categories.add(_categoryController.text);
+    searchFilter.keys.add(_keyController.text);
+
+    // search for any matching symbols
+    final results = await _symbolStyle!.searchSymbols(searchFilter);
+    final listSymbols = <_SymbolSearchPreview>[];
+    for (final result in results.toList()) {
+      final symbol = await result.getSymbol();
+      final arcgisImage = await _getSwatchImage(symbol);
+      listSymbols.add(
+        _SymbolSearchPreview(
+          name: result.name,
+          tags: result.tags,
+          symbolClass: result.symbolClass,
+          category: result.category,
+          key: result.key,
+          swatch: Image.memory(arcgisImage.getEncodedBuffer()),
+        ),
+      );
+    }
+
+    setState(() {
+      _results
+        ..clear()
+        ..addAll(listSymbols);
+      _resultCount = _results.length;
+    });
+  }
+
+  Future<ArcGISImage> _getSwatchImage(ArcGISSymbol symbol) async {
+    final screenScale = MediaQuery.devicePixelRatioOf(context);
+    final arcgisImage = await symbol.createSwatch(
+      screenScale: screenScale,
+      width: 40,
+      height: 40,
+    );
+    return arcgisImage;
+  }
+
+  Future<void> _performMockSearch() async {
+    setState(() => _ready = false);
+    await Future<void>.delayed(const Duration(milliseconds: 450));
+
+    final queryParts =
+        [
+              _nameController.text,
+              _tagController.text,
+              _symbolClassController.text,
+              _categoryController.text,
+              _keyController.text,
+            ]
+            .where((value) => value.trim().isNotEmpty)
+            .map((value) => value.trim().toLowerCase())
+            .toList();
+
+    final seeded = _mockSeedResults.where((result) {
+      if (queryParts.isEmpty) {
+        return true;
+      }
+      final haystack = [
+        result.name,
+        result.tags.join(' '),
+        result.symbolClass,
+        result.category,
+        result.key,
+      ].join(' ').toLowerCase();
+      return queryParts.every(haystack.contains);
+    }).toList();
+
+    setState(() {
+      _results
+        ..clear()
+        ..addAll(seeded);
+      _resultCount = _results.length;
+      _ready = true;
+    });
+  }
+
+  void _clear() {
+    _nameController.clear();
+    _tagController.clear();
+    _symbolClassController.clear();
+    _categoryController.clear();
+    _keyController.clear();
+
+    setState(() {
+      _results.clear();
+      _resultCount = 0;
+    });
+  }
+}
+
+class _SearchFields extends StatelessWidget {
+  const _SearchFields({
+    required this.nameController,
+    required this.tagController,
+    required this.symbolClassController,
+    required this.categoryController,
+    required this.keyController,
+  });
+
+  final TextEditingController nameController;
+  final TextEditingController tagController;
+  final TextEditingController symbolClassController;
+  final TextEditingController categoryController;
+  final TextEditingController keyController;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final wide = constraints.maxWidth >= 780;
+
+        final fields = [
+          _SearchTextField(label: 'Name', controller: nameController),
+          _SearchTextField(label: 'Tag', controller: tagController),
+          _SearchTextField(
+            label: 'Symbol class',
+            controller: symbolClassController,
+          ),
+          _SearchTextField(label: 'Category', controller: categoryController),
+          _SearchTextField(label: 'Key', controller: keyController),
+        ];
+
+        if (wide) {
+          return Wrap(
+            spacing: 12,
+            runSpacing: 12,
+            children: fields
+                .map(
+                  (field) => SizedBox(
+                    width: (constraints.maxWidth - 24) / 2,
+                    child: field,
+                  ),
+                )
+                .toList(),
+          );
+        }
+
+        return Column(
+          children: fields
+              .map(
+                (field) => Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: field,
+                ),
+              )
+              .toList(),
+        );
+      },
+    );
+  }
+}
+
+class _SearchTextField extends StatelessWidget {
+  const _SearchTextField({required this.label, required this.controller});
+
+  final String label;
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        border: const OutlineInputBorder(),
+        isDense: true,
+      ),
+    );
+  }
+}
+
+class _SearchResultCard extends StatelessWidget {
+  const _SearchResultCard({required this.result});
+
+  final _SymbolSearchPreview result;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Card(
+      margin: EdgeInsets.zero,
+      clipBehavior: Clip.antiAlias,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 46,
+              height: 46,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondaryContainer,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: result.swatch == null
+                  ? const Icon(Icons.military_tech)
+                  : result.swatch!,
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(result.name, style: textTheme.titleSmall),
+                  const SizedBox(height: 3),
+                  Text(
+                    'Tags: ${result.tags.join(', ')}',
+                    style: textTheme.bodySmall,
+                  ),
+                  Text(
+                    'Symbol class: ${result.symbolClass}',
+                    style: textTheme.bodySmall,
+                  ),
+                  Text(
+                    'Category: ${result.category}',
+                    style: textTheme.bodySmall,
+                  ),
+                  Text('Key: ${result.key}', style: textTheme.bodySmall),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SymbolSearchPreview {
+  const _SymbolSearchPreview({
+    required this.name,
+    required this.tags,
+    required this.symbolClass,
+    required this.category,
+    required this.key,
+    this.swatch,
+  });
+
+  final String name;
+  final List<String> tags;
+  final String symbolClass;
+  final String category;
+  final String key;
+  final Image? swatch;
+}
+
+const _mockSeedResults = [
+  _SymbolSearchPreview(
+    name: 'Infantry Platoon',
+    tags: ['ground', 'friendly', 'maneuver'],
+    symbolClass: 'Unit',
+    category: 'Ground Units',
+    key: 'SFGPUCI----K---',
+  ),
+  _SymbolSearchPreview(
+    name: 'Medical Evacuation',
+    tags: ['support', 'medical', 'mobility'],
+    symbolClass: 'Unit',
+    category: 'Medical',
+    key: 'SFGPUUMED--K---',
+  ),
+  _SymbolSearchPreview(
+    name: 'Reconnaissance Team',
+    tags: ['ground', 'sensor', 'intelligence'],
+    symbolClass: 'Unit',
+    category: 'Recon',
+    key: 'SFGPUUR----K---',
+  ),
+  _SymbolSearchPreview(
+    name: 'Forward Operating Base',
+    tags: ['installation', 'ground', 'logistics'],
+    symbolClass: 'Installation',
+    category: 'Facilities',
+    key: 'GFGPGPP----A---',
+  ),
+  _SymbolSearchPreview(
+    name: 'Air Defense Battery',
+    tags: ['air', 'friendly', 'defense'],
+    symbolClass: 'Unit',
+    category: 'Air Defense',
+    key: 'SFGPUCAD---K---',
+  ),
+];
