65edf8e6189eaafff8bce544124273d0fac1f30e
Martino Yovo <martinosoyen@gmail.com>
2026-06-15 10:34:55 -0700

    martinoyovo/clean up (#520)
    * Clean up
    
    * Clean up
    
    * Update metadata

--- lib/samples/show_device_location_with_nmea_data_sources/show_device_location_with_nmea_data_sources.dart
+++ lib/samples/show_device_location_with_nmea_data_sources/show_device_location_with_nmea_data_sources.dart
@@ -18,7 +18,7 @@ import 'dart:io';
 
 import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:arcgis_maps_sdk_flutter_samples/common/common.dart';
-import 'package:arcgis_maps_sdk_flutter_samples/samples/show_device_location_with_nmea_data_sources/simulated_nmea_data_source.dart';
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -46,7 +46,7 @@ class _ShowDeviceLocationWithNmeaDataSourcesState
   var _currentSatelliteInfos = <NmeaSatelliteInfo>[];
 
   // Simulated NMEA data provider members.
-  SimulatedNmeaDataSource? _nmeaDataSimulator;
+  _SimulatedNmeaDataSource? _nmeaDataSimulator;
   StreamSubscription<String>? _nmeaDataSubscription;
 
   // Enable or disable the Recenter button.
@@ -179,7 +179,7 @@ class _ShowDeviceLocationWithNmeaDataSourcesState
     final nmeaSentences = await _loadNmeaFile();
 
     // Create new instance of the NmeaSourceSimulator.
-    _nmeaDataSimulator ??= SimulatedNmeaDataSource(nmeaSentences);
+    _nmeaDataSimulator ??= _SimulatedNmeaDataSource(nmeaSentences);
 
     // Subscribe to the simulator data.
     _nmeaDataSubscription ??= _nmeaDataSimulator!.nmeaMessages.listen((
@@ -318,5 +318,91 @@ extension on NmeaGnssSystem {
       default:
         return 'Unknown GNSS type';
     }
+  }
+}
+
+class _SimulatedNmeaDataSource {
+  // Simulates timed groups of NMEA sentences for the location data source.
+  _SimulatedNmeaDataSource(this.nmeaSentences)
+    : _interval = const Duration(seconds: 1) {
+    _nmeaMessagesController.onListen = _start;
+    _nmeaMessagesController.onCancel = _shutdown;
+
+    // Create the timer for posting the messages.
+    _timer = RestartableTimer(_interval, _postNextMessageBlock);
+  }
+
+  final Duration _interval;
+  late final RestartableTimer _timer;
+  var _running = false;
+  var _currentNmeaBlockIndex = 0;
+  final _sentencesByTimeBlock = <String>[];
+  final List<String> nmeaSentences;
+  // Stream to provide the NMEA messages.
+  final _nmeaMessagesController = StreamController<String>();
+  Stream<String> get nmeaMessages => _nmeaMessagesController.stream;
+
+  // Function to post the next block of NMEA sentences in the list to the stream.
+  // If the end of the list is reached, the counter restarts from the beginning.
+  void _postNextMessageBlock() {
+    if (!_running) return;
+
+    _nmeaMessagesController.add(_sentencesByTimeBlock[_currentNmeaBlockIndex]);
+    // Increment the index. If the end is reached, start back at 0.
+    _currentNmeaBlockIndex =
+        _currentNmeaBlockIndex + 1 == _sentencesByTimeBlock.length
+        ? 0
+        : _currentNmeaBlockIndex + 1;
+
+    _timer.reset();
+  }
+
+  // Download the sample NMEA file and read the contents into a list of messages
+  // that combine all NMEA sentences for the same time into a single message string.
+  Future<void> _initData() async {
+    final messageSentences = <String>[];
+    for (final sentence in nmeaSentences) {
+      final sentenceComponents = sentence.split(',');
+      // In this test data set, every block of sentences for a certain time starts with a GGA sentence.
+      if (sentenceComponents[0].contains('GGA')) {
+        if (messageSentences.isNotEmpty) {
+          // Close out the prior block of sentences.
+          // Join the sentences with a linefeed. Append an additional
+          // linefeed to the end to properly terminate the last sentence.
+          final groupedSentences = '${messageSentences.join('\n')}\n';
+          _sentencesByTimeBlock.add(groupedSentences);
+          messageSentences.clear();
+        }
+      }
+
+      messageSentences.add(sentence);
+    }
+  }
+
+  // Initialize the data and restart the timer.
+  Future<void> _start() async {
+    if (_running) return;
+
+    if (_sentencesByTimeBlock.isEmpty) {
+      // Load the NMEA data.
+      await _initData();
+    }
+
+    _running = true;
+
+    // Post the first block of sentences immediately.
+    _postNextMessageBlock();
+
+    // Reset the timer for the next interval.
+    _timer.reset();
+  }
+
+  // Stops the simulator. Cancels the running timer and closes the stream.
+  void _shutdown() {
+    if (!_running) return;
+
+    _nmeaMessagesController.close().ignore();
+    if (_timer.isActive) _timer.cancel();
+    _running = false;
   }
 }
