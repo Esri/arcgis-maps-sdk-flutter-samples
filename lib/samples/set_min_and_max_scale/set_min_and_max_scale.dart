43c5dec41a0848708e69c21f9912a76225102148
kareemhassen <kar15074@esri.com>
2026-07-02 16:13:30 -0700

    Set Min and Max Scale sample

---
+++ lib/samples/set_min_and_max_scale/set_min_and_max_scale.dart
@@ -0,0 +1,71 @@
+// Copyright 2026 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:arcgis_maps_sdk_flutter_samples/common/common.dart';
+import 'package:flutter/material.dart';
+
+class SetMinAndMaxScale extends StatefulWidget {
+  const SetMinAndMaxScale({super.key});
+
+  @override
+  State<SetMinAndMaxScale> createState() => _SetMinAndMaxScaleState();
+}
+
+class _SetMinAndMaxScaleState extends State<SetMinAndMaxScale>
+    with SampleStateSupport {
+  // Create a controller for the map view.
+  final _mapViewController = ArcGISMapView.createController();
+  // A flag for when the map view is ready and controls can be used.
+  var _ready = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Add a map view to the widget tree and set a controller.
+          ArcGISMapView(
+            controllerProvider: () => _mapViewController,
+            onMapViewReady: onMapViewReady,
+          ),
+          // Display a progress indicator and prevent interaction until state is ready.
+          LoadingIndicator(visible: !_ready),
+        ],
+      ),
+    );
+  }
+
+  Future<void> onMapViewReady() async {
+    // Create a map with a basemap style and initial viewpoint.
+    final map = ArcGISMap.withBasemapStyle(.arcGISTopographic);
+    map.initialViewpoint = Viewpoint.fromCenter(
+      ArcGISPoint(
+        x: -355453,
+        y: 7548720,
+        spatialReference: SpatialReference.webMercator,
+      ),
+      scale: 3000,
+    );
+    // Set min and max scales of map.
+    map.minScale = 800;
+    map.maxScale = 2000;
+    // Set the map to the map view controller.
+    _mapViewController.arcGISMap = map;
+
+    // Set the ready state variable to true to enable the sample UI.
+    setState(() => _ready = true);
+  }
+}
